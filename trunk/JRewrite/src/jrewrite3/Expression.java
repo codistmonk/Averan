986d5dfb8972e82552f16bf8748e62b32705c5cc
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-01 07:31:34 +0000

    [JRewrite][jrewrite3] Added VisitorTest.

--- trunk/JRewrite/src/jrewrite3/Expression.java
+++ trunk/JRewrite/src/jrewrite3/Expression.java
@@ -1,7 +1,10 @@
 package jrewrite3;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author codistmonk (creation 2014-08-01)
@@ -10,6 +13,10 @@ public abstract interface Expression extends Serializable {
 	
 	public abstract Object accept(Visitor visitor);
 	
+	public static List<Object> listAccept(final Collection<? extends Expression> expressions, final Visitor visitor) {
+		return expressions.stream().map(e -> e.accept(visitor)).collect(Collectors.toList());
+	}
+	
 }
 
 /**
@@ -17,7 +24,24 @@ public abstract interface Expression extends Serializable {
  */
 final class Composite implements Expression {
 	
-	private final List<Expression> children;
+	private final List<Expression> children = new ArrayList<>();
+	
+	public final List<Expression> getChildren() {
+		return this.children;
+	}
+	
+	@Override
+	public final Object accept(final Visitor visitor) {
+		final Object beforeVisit = visitor.visitBeforeChildren(this);
+		final List<Object> childVisits = Expression.listAccept(this.getChildren(), visitor);
+		
+		return visitor.visitAfterChildren(this, beforeVisit, childVisits);
+	}
+	
+	/**
+	 * {@value}.
+	 */
+	private static final long serialVersionUID = -280050093929737583L;
 	
 }
 
@@ -30,13 +54,76 @@ final class Module implements Expression {
 	
 	private final List<Variable> variables;
 	
+	private final List<Expression> conditions;
+	
+	private final List<Expression> facts;
+	
+	public Module(final Module parent) {
+		this.parent = parent;
+		this.variables = new ArrayList<>();
+		this.conditions = new ArrayList<>();
+		this.facts = new ArrayList<>();
+	}
+	
+	public final Module getParent() {
+		return this.parent;
+	}
+	
+	public final List<Variable> getVariables() {
+		return this.variables;
+	}
+	
+	public final List<Expression> getConditions() {
+		return this.conditions;
+	}
+	
+	public final List<Expression> getFacts() {
+		return this.facts;
+	}
+	
+	@Override
+	public final Object accept(final Visitor visitor) {
+		final Object beforeVisit = visitor.visitBeforeVariables(this);
+		final List<Object> variableVisits = Expression.listAccept(this.getVariables(), visitor);
+		final List<Object> conditionVisits = Expression.listAccept(this.getConditions(), visitor);
+		final List<Object> factVisits = Expression.listAccept(this.getFacts(), visitor);
+		
+		return visitor.visitAfterFacts(this, beforeVisit, variableVisits, conditionVisits, factVisits);
+	}
+	
 	/**
 	 * @author codistmonk (creation 2014-08-01)
 	 */
 	public final class Variable implements Expression {
 		
+		private final String string;
+		
+		public Variable(final String string) {
+			this.string = string;
+		}
+		
+		@Override
+		public final String toString() {
+			return this.string;
+		}
+		
+		@Override
+		public final Object accept(final Visitor visitor) {
+			return visitor.visit(this);
+		}
+		
+		/**
+		 * {@value}.
+		 */
+		private static final long serialVersionUID = 2038510531251596976L;
+		
 	}
 	
-	public static final Module ROOT = new Module();
+	/**
+	 * {@value}.
+	 */
+	private static final long serialVersionUID = -6696557631458945912L;
+	
+	public static final Module ROOT = new Module(null);
 	
 }
---
+++ trunk/JRewrite/test/jrewrite3/VisitorTest.java
@@ -0,0 +1,103 @@
+package jrewrite3;
+
+import static java.util.Arrays.asList;
+import static jrewrite3.VisitorTest.Recorder.Event.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jrewrite3.Module.Variable;
+
+import org.junit.Test;
+
+/**
+ * @author codistmonk (creation 2014-08-01)
+ */
+public final class VisitorTest {
+	
+	@Test
+	public final void test1() {
+		final Recorder recorder = new Recorder();
+		
+		assertEquals(Module.ROOT, Module.ROOT.accept(recorder));
+		assertEquals(asList(MODULE_BEFORE_VARIABLES, MODULE_AFTER_FACTS), recorder.getEvents());
+	}
+	
+	/**
+	 * @author codistmonk (creation 2014-08-01)
+	 */
+	public static final class Recorder implements Visitor {
+		
+		private final List<Event> events = new ArrayList<>();
+		
+		public final List<Event> getEvents() {
+			return this.events;
+		}
+		
+		@Override
+		public final Object visitBeforeChildren(final Composite composite) {
+			this.getEvents().add(Event.COMPOSITE_BEFORE_CHILDREN);
+			
+			return composite;
+		}
+		
+		@Override
+		public final Object visitAfterChildren(final Composite composite,
+				final Object beforeVisit, final List<Object> childVisits) {
+			this.getEvents().add(Event.COMPOSITE_AFTER_CHILDREN);
+			
+			assertEquals(composite, beforeVisit);
+			assertEquals(composite.getChildren(), childVisits);
+			
+			return Visitor.super.visitAfterChildren(composite, beforeVisit, childVisits);
+		}
+		
+		@Override
+		public final Object visit(final Variable variable) {
+			this.getEvents().add(Event.VARIABLE);
+			
+			return variable;
+		}
+		
+		@Override
+		public final Object visitBeforeVariables(final Module module) {
+			this.getEvents().add(Event.MODULE_BEFORE_VARIABLES);
+			
+			return module;
+		}
+		
+		@Override
+		public final Object visitAfterFacts(final Module module, final Object beforeVisit,
+				final List<Object> variableVisits, final List<Object> conditionVisits,
+				final List<Object> factVisits) {
+			this.getEvents().add(Event.MODULE_AFTER_FACTS);
+			
+			assertEquals(module, beforeVisit);
+			assertEquals(module.getVariables(), variableVisits);
+			assertEquals(module.getConditions(), conditionVisits);
+			assertEquals(module.getFacts(), factVisits);
+			
+			return Visitor.super.visitAfterFacts(module, beforeVisit, variableVisits,
+					conditionVisits, factVisits);
+		}
+		
+		/**
+		 * {@value}.
+		 */
+		private static final long serialVersionUID = -7418401972134012397L;
+		
+		/**
+		 * @author codistmonk (creation 2014-08-01)
+		 */
+		public static enum Event {
+			
+			COMPOSITE_BEFORE_CHILDREN, COMPOSITE_AFTER_CHILDREN,
+			MODULE_BEFORE_VARIABLES, MODULE_AFTER_FACTS,
+			VARIABLE;
+			
+		}
+		
+	}
+	
+}
