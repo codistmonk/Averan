d899b33ac91addef7678ba61cba57c21a9f6b5db
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-01 07:31:34 +0000

    [JRewrite][jrewrite3] Added VisitorTest.

--- JRewrite/src/jrewrite3/Visitor.java
+++ JRewrite/src/jrewrite3/Visitor.java
@@ -1,8 +1,27 @@
 package jrewrite3;
 
+import java.io.Serializable;
+import java.util.List;
+
 /**
  * @author codistmonk (creation 2014-08-01)
  */
-public abstract interface Visitor {
-
+public abstract interface Visitor extends Serializable {
+	
+	public abstract Object visitBeforeChildren(Composite composite);
+	
+	public default Object visitAfterChildren(final Composite composite, final Object beforeVisit,
+			final List<Object> childVisits) {
+		return beforeVisit;
+	}
+	
+	public abstract Object visit(Module.Variable variable);
+	
+	public abstract Object visitBeforeVariables(Module module);
+	
+	public default Object visitAfterFacts(final Module module, final Object beforeVisit,
+			final List<Object> variableVisits, final List<Object> conditionVisits, final List<Object> factVisits) {
+		return beforeVisit;
+	}
+	
 }
