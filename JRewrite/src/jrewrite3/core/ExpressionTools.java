c81827dfbed889f2b99fd6106222197e44b8cb0a
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-05 01:42:39 +0000

    [JRewrite][jrewrite3] Started work on Demo2.

--- JRewrite/src/jrewrite3/core/ExpressionTools.java
+++ JRewrite/src/jrewrite3/core/ExpressionTools.java
@@ -2,13 +2,16 @@ package jrewrite3.core;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
+import static net.sourceforge.aprog.tools.Tools.cast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 
 import jrewrite3.core.Module.Symbol;
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
+import net.sourceforge.aprog.tools.Tools;
 
 /**
  * @author codistmonk (creation 2014-08-04)
@@ -27,14 +30,18 @@ public final class ExpressionTools {
 				? (Expression) object : nullModule.new Symbol(object.toString());
 		
 		if (objects.length == 1) {
-			
 			return (E) mapper.apply(objects[0]);
 		}
 		
 		if (objects.length == 2 && objects[0] instanceof List) {
-			final Module rule = (Module) objects[1];
+			final Module rule = cast(Module.class, objects[1]);
 			
-			return (E) new Module(null, (List<Symbol>) objects[0], rule.getConditions(), rule.getFacts());
+			if (rule != null) {
+				return (E) new Module(null, (List<Symbol>) objects[0], rule.getConditions(), rule.getFacts());
+			}
+			
+			return (E) new Module(null, (List<Symbol>) objects[0], new ArrayList<>(),
+					new ArrayList<>(Arrays.asList($(objects[1]))));
 		}
 		
 		if (objects.length == 3) {
