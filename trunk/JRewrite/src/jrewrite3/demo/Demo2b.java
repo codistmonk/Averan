7b8816739e48db84bd351cfe86bfe79c6df47863
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-11 00:14:40 +0000

    [JRewrite][jrewrite3] Refactoring.

--- trunk/JRewrite/src/jrewrite3/demo/Demo2b.java
+++ trunk/JRewrite/src/jrewrite3/demo/Demo2b.java
@@ -109,117 +109,10 @@ public final class Demo2b {
 			
 			claimCommutativityOfConjunction(session);
 			claimTranspositionOfAddition(session);
-			
-			session.claim("transposition_of_multiplication", $$("∀X,Y ((X∈≀M_(`rowCount_X,`columnCount_X)) → ((Y∈≀M_(`rowCount_Y,`columnCount_Y)) → ((`columnCount_X=`rowCount_Y) → ((XY)ᵀ=YᵀXᵀ))))"));
-			
-			{
-				session.introduce();
-				session.introduce();
-				session.introduce();
-				session.introduce();
-				session.introduce();
-				
-				final Symbol x = session.getParameter("X");
-				final Symbol y = session.getParameter("Y");
-				final Expression xt = $(x, "ᵀ");
-				final Expression yt = $(y, "ᵀ");
-				final Expression xy = $(x, y);
-				final Expression xyt = $(xy, "ᵀ");
-				final Expression ytxt = $(yt, xt);
-				
-				session.bind("definition_of_matrix_equality", xyt, ytxt);
-				
-				session.claim(((Composite) session.getLastFact()).get(2));
-				
-				{
-					session.introduce();
-					session.introduce();
-					
-					final Symbol i = session.getParameter("i");
-					final Symbol j = session.getParameter("j");
-					final Symbol k = session.getCurrentModule().new Symbol("k");
-					final Expression rowCountX = $("rowCount", "_", x);
-					final Expression columnCountX = $("columnCount", "_", x);
-					final Expression rowCountY = $("rowCount", "_", y);
-					final Expression columnCountY = $("columnCount", "_", y);
-					
-					session.bind(Standard.IDENTITY, k);
-					session.bind("definition_of_transposition", xy, i, j);
-					session.bind("definition_of_matrix_multiplication", x, y, columnCountX);
-					session.claim(((Module) session.getLastFact()).getConditions().get(0));
-					
-					{
-						session.bind(Standard.IDENTITY, columnCountX);
-						session.rewrite(session.getLastFactName(), "transposition_of_multiplication#2", 0);
-					}
-					
-					session.apply("transposition_of_multiplication#4#2", session.getLastFactName());
-					session.bind(session.getLastFactName(), j, i, k);
-					session.rewrite("transposition_of_multiplication#4#1", session.getLastFactName());
-					
-					session.bind("definition_of_matrix_multiplication", yt, xt, $("columnCount", "_", x));
-					session.claim(((Module) session.getLastFact()).getConditions().get(0));
-					
-					{
-						session.bind("definition_of_transposition_columnCount", y);
-						rewriteRight(session, session.getLastFactName(), "transposition_of_multiplication#2");
-						session.bind("definition_of_transposition_rowCount", x);
-					}
-					
-					session.apply("transposition_of_multiplication#4#7", session.getLastFactName());
-					session.bind(session.getLastFactName(), i, j, k);
-					
-					session.bind("definition_of_transposition", x);
-					session.bind(session.getLastFactName(), k, j);
-					session.rewrite("transposition_of_multiplication#4#10", session.getLastFactName());
-					
-					session.bind("definition_of_transposition", y);
-					session.bind(session.getLastFactName(), i, k);
-					session.rewrite("transposition_of_multiplication#4#13", session.getLastFactName());
-					
-					final Expression xjk = $(x, "_", $(j, ",", k));
-					final Expression yki = $(y, "_", $(k, ",", i));
-					final Expression xjkyki = $(xjk, yki);
-					final Expression ykixjk = $(yki, xjk);
-					
-					session.claim($(ykixjk, "=", xjkyki));
-					
-					{
-						session.claim($(xjk, "∈", "ℝ"));
-						
-						{
-							session.bind("definition_of_matrices", x, rowCountX, columnCountX);
-							session.rewrite("transposition_of_multiplication#0", session.getLastFactName());
-							session.bind(session.getLastFactName());
-							session.bind(session.getLastFactName(), j, k);
-						}
-						
-						session.claim($(yki, "∈", "ℝ"));
-						
-						{
-							session.bind("definition_of_matrices", y, rowCountY, columnCountY);
-							session.rewrite("transposition_of_multiplication#1", session.getLastFactName());
-							session.bind(session.getLastFactName());
-							session.bind(session.getLastFactName(), k, i);
-						}
-						
-						session.bind("commutativity_of_multiplication", yki, xjk);
-						session.apply(session.getLastFactName(), "transposition_of_multiplication#4#17#1");
-						session.apply(session.getLastFactName(), "transposition_of_multiplication#4#17#0");
-					}
-					
-					session.rewrite("transposition_of_multiplication#4#16", session.getLastFactName());
-					rewriteRight(session, "transposition_of_multiplication#4#6", session.getLastFactName());
-					
-				}
-				
-				rewriteRight(session, session.getLastFactName(), "transposition_of_multiplication#3");
-				if (true) break block;
-			}
+			claimTranspositionOfMultiplication(session);
 			
 			if (true) break block;
 			
-			session.admit("transposition_of_multiplication", $$("∀X,Y ((`columnCount_X=`rowCount_Y) → ((XY)ᵀ=YᵀXᵀ))"));
 			session.admit("definition_of_replicated_means", $$("∀X,n ((`columnCount_X=n) → (M_X)=X(U_n)(U_n)ᵀ)"));
 			session.admit("simplified_definition_of_variance", $$("∀X ((`Var_X)=(XXᵀ)-((M_X)(M_X)ᵀ))"));
 			session.admit("definition_of_problem_dimension", $$("0<D"));
@@ -238,9 +131,118 @@ public final class Demo2b {
 		{
 			final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 			
-			session.new Exporter(new TexPrinter(buffer), -1).exportSession();
+			session.new Exporter(new TexPrinter(buffer), 0).exportSession();
 			
 			new TeXFormula(buffer.toString()).createPNG(0, 18F, "view.png", WHITE, BLACK);
+		}
+	}
+	
+	public static final void claimTranspositionOfMultiplication(final Session session) {
+		session.claim("transposition_of_multiplication",
+				$$("∀X,Y ((X∈≀M_(`rowCount_X,`columnCount_X)) → ((Y∈≀M_(`rowCount_Y,`columnCount_Y)) → ((`columnCount_X=`rowCount_Y) → ((XY)ᵀ=YᵀXᵀ))))"));
+		
+		{
+			session.introduce();
+			session.introduce();
+			session.introduce();
+			session.introduce();
+			session.introduce();
+			
+			final Symbol x = session.getParameter("X");
+			final Symbol y = session.getParameter("Y");
+			final Expression xt = $(x, "ᵀ");
+			final Expression yt = $(y, "ᵀ");
+			final Expression xy = $(x, y);
+			final Expression xyt = $(xy, "ᵀ");
+			final Expression ytxt = $(yt, xt);
+			
+			session.bind("definition_of_matrix_equality", xyt, ytxt);
+			
+			session.claim(((Composite) session.getLastFact()).get(2));
+			
+			{
+				session.introduce();
+				session.introduce();
+				
+				final Symbol i = session.getParameter("i");
+				final Symbol j = session.getParameter("j");
+				final Symbol k = session.getCurrentModule().new Symbol("k");
+				final Expression rowCountX = $("rowCount", "_", x);
+				final Expression columnCountX = $("columnCount", "_", x);
+				final Expression rowCountY = $("rowCount", "_", y);
+				final Expression columnCountY = $("columnCount", "_", y);
+				
+				session.bind(Standard.IDENTITY, k);
+				session.bind("definition_of_transposition", xy, i, j);
+				session.bind("definition_of_matrix_multiplication", x, y, columnCountX);
+				session.claim(((Module) session.getLastFact()).getConditions().get(0));
+				
+				{
+					session.bind(Standard.IDENTITY, columnCountX);
+					session.rewrite(session.getLastFactName(), "transposition_of_multiplication#2", 0);
+				}
+				
+				session.apply("transposition_of_multiplication#4#2", session.getLastFactName());
+				session.bind(session.getLastFactName(), j, i, k);
+				session.rewrite("transposition_of_multiplication#4#1", session.getLastFactName());
+				
+				session.bind("definition_of_matrix_multiplication", yt, xt, $("columnCount", "_", x));
+				session.claim(((Module) session.getLastFact()).getConditions().get(0));
+				
+				{
+					session.bind("definition_of_transposition_columnCount", y);
+					rewriteRight(session, session.getLastFactName(), "transposition_of_multiplication#2");
+					session.bind("definition_of_transposition_rowCount", x);
+				}
+				
+				session.apply("transposition_of_multiplication#4#7", session.getLastFactName());
+				session.bind(session.getLastFactName(), i, j, k);
+				
+				session.bind("definition_of_transposition", x);
+				session.bind(session.getLastFactName(), k, j);
+				session.rewrite("transposition_of_multiplication#4#10", session.getLastFactName());
+				
+				session.bind("definition_of_transposition", y);
+				session.bind(session.getLastFactName(), i, k);
+				session.rewrite("transposition_of_multiplication#4#13", session.getLastFactName());
+				
+				final Expression xjk = $(x, "_", $(j, ",", k));
+				final Expression yki = $(y, "_", $(k, ",", i));
+				final Expression xjkyki = $(xjk, yki);
+				final Expression ykixjk = $(yki, xjk);
+				
+				session.claim($(ykixjk, "=", xjkyki));
+				
+				{
+					session.claim($(xjk, "∈", "ℝ"));
+					
+					{
+						session.bind("definition_of_matrices", x, rowCountX, columnCountX);
+						session.rewrite("transposition_of_multiplication#0", session.getLastFactName());
+						session.bind(session.getLastFactName());
+						session.bind(session.getLastFactName(), j, k);
+					}
+					
+					session.claim($(yki, "∈", "ℝ"));
+					
+					{
+						session.bind("definition_of_matrices", y, rowCountY, columnCountY);
+						session.rewrite("transposition_of_multiplication#1", session.getLastFactName());
+						session.bind(session.getLastFactName());
+						session.bind(session.getLastFactName(), k, i);
+					}
+					
+					session.bind("commutativity_of_multiplication", yki, xjk);
+					session.apply(session.getLastFactName(), "transposition_of_multiplication#4#17#1");
+					session.apply(session.getLastFactName(), "transposition_of_multiplication#4#17#0");
+				}
+				
+				session.rewrite("transposition_of_multiplication#4#16", session.getLastFactName());
+				rewriteRight(session, "transposition_of_multiplication#4#6", session.getLastFactName());
+				
+			}
+			
+			rewriteRight(session, session.getLastFactName(), "transposition_of_multiplication#3");
 		}
 	}
 	
