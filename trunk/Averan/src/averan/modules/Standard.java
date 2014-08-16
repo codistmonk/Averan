739432d70673f0220624610744e30ceaf7c492aa
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-16 08:04:02 +0000

    [Averan] Refactoring.

--- trunk/Averan/src/averan/modules/Standard.java
+++ trunk/Averan/src/averan/modules/Standard.java
@@ -2,24 +2,15 @@ package averan.modules;
 
 import static averan.core.ExpressionTools.*;
 import static averan.core.Module.ROOT;
-import static averan.core.SessionTools.claim;
-import static averan.core.SessionTools.session;
+import static averan.core.SessionTools.*;
 import static net.sourceforge.aprog.tools.Tools.ignore;
 
 import averan.core.Composite;
 import averan.core.Expression;
 import averan.core.Module;
 import averan.core.Session;
-import averan.core.Module.Bind;
 import averan.core.Rewriter;
-import averan.core.Visitor;
 import averan.core.Module.Symbol;
-import averan.core.Pattern;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
 
@@ -124,100 +115,6 @@ public final class Standard {
 		{
 			unifyAndApply(session, RECALL, propositionName);
 		}
-	}
-	
-	public static final void unifyAndApply(final String moduleName, final String conditionName) {
-		unifyAndApply(session(), moduleName, conditionName);
-	}
-	
-	public static final void unifyAndApply(final Session session, final String moduleName, final String conditionName) {
-		final Module module = session.getProposition(moduleName);
-		final Pattern anyfiedCondition = anyfy(module.getConditions().get(0));
-		
-		if (!anyfiedCondition.equals(session.getProposition(conditionName))) {
-			throw new IllegalArgumentException("Failed to unify (" + conditionName + ") with " + anyfiedCondition.getTemplate());
-		}
-		
-		computeBind(session.getCurrentModule(), module, moduleName, anyfiedCondition).execute();
-		
-		session.apply(session.getFactName(-1), conditionName);
-	}
-	
-	private static final Bind computeBind(final Module context, final Module module, final String moduleName, final Pattern anyfiedCondition) {
-		final Bind result = context.new Bind(context, moduleName);
-		
-		for (final Symbol parameter : module.getParameters()) {
-			final String parameterName = parameter.toString();
-			
-			result.bind(parameterName, anyfiedCondition.get(parameterName));
-		}
-		
-		return result;
-	}
-	
-	public static final Pattern anyfy(final Expression expression) {
-		final Map<String, List<Module>> parameterNameUsage = new HashMap<>();
-		final Pattern result = new Pattern(expression.accept(new Visitor<Expression>() {
-			
-			@Override
-			public final Expression visit(final Composite composite) {
-				return new Composite(composite.childrenAcceptor(this).get());
-			}
-			
-			@Override
-			public final Expression visit(final Symbol symbol) {
-				return Pattern.any(symbol.toString());
-			}
-			
-			@Override
-			public final Expression visit(final Module module) {
-				final Module result = new Module(module.getParent(), module.getName(),
-						new ArrayList<>(module.getParameters()), new ArrayList<>(module.getConditions()), new ArrayList<>(module.getFacts()));
-				final Rewriter rewriter = new Rewriter();
-				
-				{
-					final List<Symbol> parameters = result.getParameters();
-					final int n = parameters.size();
-					
-					for (int i = 0; i < n; ++i) {
-						final Symbol parameter = parameters.get(i);
-						final List<Module> usage = parameterNameUsage.compute(parameter.toString(), (k, v) -> v == null ? new ArrayList<>() : v );
-						
-						usage.add(result);
-						
-						rewriter.rewrite(parameter, Pattern.any(parameter.toString(), usage.size()));
-					}
-				}
-				
-				{
-					final List<Expression> conditions = result.getConditions();
-					final int n = conditions.size();
-					
-					for (int i = 0; i < n; ++i) {
-						conditions.set(i, conditions.get(i).accept(rewriter));
-					}
-				}
-				
-				{
-					final List<Expression> facts = result.getFacts();
-					final int n = facts.size();
-					
-					for (int i = 0; i < n; ++i) {
-						facts.set(i, facts.get(i).accept(rewriter));
-					}
-				}
-				
-				return result;
-			}
-			
-			/**
-			 * {@value}.
-			 */
-			private static final long serialVersionUID = -7233860227717949061L;
-			
-		}));
-		
-		return result;
 	}
 	
 }
