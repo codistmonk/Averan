13956bd77286d6bf953bb134db4f44f3f76cf22f
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-22 03:28:02 +0000

    [Averan] Updated Demo2.

--- trunk/Averan/src/averan/core/Rewriter.java
+++ trunk/Averan/src/averan/core/Rewriter.java
@@ -1,5 +1,7 @@
 package averan.core;
 
+import static net.sourceforge.aprog.tools.Tools.cast;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -10,6 +12,7 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.function.Supplier;
 
+import net.sourceforge.aprog.tools.Tools;
 import averan.core.Module.Statement;
 import averan.core.Module.Symbol;
 
@@ -71,7 +74,9 @@ public final class Rewriter implements Visitor<Expression> {
 		if (composite == compositeVisit) {
 			final List<Expression> childVisits = composite.childrenAcceptor(this).get();
 			
+			Tools.debugPrint(composite, childVisits);
 			if (!equals2(composite.getChildren(), childVisits)) {
+				Tools.debugPrint(composite, childVisits);
 				return new Composite(childVisits);
 			}
 		}
@@ -95,7 +100,10 @@ public final class Rewriter implements Visitor<Expression> {
 				return false;
 			}
 			
-			if (expression1 instanceof Pattern.Any && !(expression2 instanceof Pattern.Any)) {
+			Tools.debugPrint(expression1, expression1.getClass());
+			Tools.debugPrint(expression2, expression2.getClass());
+			
+			if ((expression1 instanceof Pattern.Any) != (expression2 instanceof Pattern.Any)) {
 				return false;
 			}
 		}
@@ -103,9 +111,73 @@ public final class Rewriter implements Visitor<Expression> {
 		return true;
 	}
 	
+	/**
+	 * @author codistmonk (creation 2014-08-22)
+	 *
+	 * @param <T>
+	 */
+	public static abstract class Zipper<T> implements Visitor<T> {
+		
+		private Expression other;
+		
+		@Override
+		public final T visit(final Composite composite) {
+			final Composite otherComposite = cast(Composite.class, this.other);
+			
+			if (otherComposite == null) {
+				return null;
+			}
+			
+			final List<T> childVisits = new ArrayList<>();
+			
+			{
+				final List<Expression> compositeChildren = composite.getChildren();
+				final List<Expression> otherChildren = otherComposite.getChildren();
+				final int n = compositeChildren.size();
+				
+				if (otherChildren.size() != n) {
+					return null;
+				}
+				
+				for (int i = 0; i < n; ++i) {
+					this.other = otherChildren.get(i);
+					final T childVisit = compositeChildren.get(i).accept(this);
+					
+					if (childVisit == null) {
+						return null;
+					}
+					
+					childVisits.add(childVisit);
+				}
+			}
+			
+			return this.endVisit(composite, childVisits);
+		}
+		
+		@Override
+		public final T visit(final Module module) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		public final T visit(final Symbol symbol) {
+			
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		protected abstract T endVisit(Composite composite, List<T> childVisits);
+		
+	}
+	
 	@Override
 	public final Expression visit(final Pattern.Any any) {
-		return this.tryToRewrite(any);
+		final Expression result = this.tryToRewrite(any);
+		
+		Tools.debugPrint(any, result);
+		
+		return result;
 	}
 	
 	@Override
