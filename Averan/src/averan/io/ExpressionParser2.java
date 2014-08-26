f021335116ddad92cb02f36b4a5738b8b5300f96
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-26 17:39:40 +0000

    [Averan] Updated Aurochs jar; refactoring.

--- Averan/src/averan/io/ExpressionParser2.java
+++ Averan/src/averan/io/ExpressionParser2.java
@@ -2,7 +2,6 @@ package averan.io;
 
 import static averan.core.ExpressionTools.*;
 import static java.util.Arrays.copyOfRange;
-import static net.sourceforge.aprog.tools.Tools.array;
 import static net.sourceforge.aprog.tools.Tools.cast;
 import static net.sourceforge.aurochs2.core.LexerBuilder.*;
 import static net.sourceforge.aurochs2.core.ParserBuilder.block;
@@ -10,6 +9,7 @@ import static net.sourceforge.aurochs2.core.ParserBuilder.Priority.Associativity
 import static net.sourceforge.aurochs2.core.ParserBuilder.Priority.Associativity.NONE;
 import static net.sourceforge.aurochs2.core.ParserBuilder.Priority.Associativity.RIGHT;
 import static net.sourceforge.aurochs2.core.TokenSource.tokens;
+
 import averan.core.Expression;
 
 import java.io.Serializable;
@@ -17,123 +17,27 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
-import net.sourceforge.aprog.tools.Tools;
 import net.sourceforge.aurochs2.core.Grammar.Rule;
 import net.sourceforge.aurochs2.core.LRParser;
 import net.sourceforge.aurochs2.core.Lexer;
 import net.sourceforge.aurochs2.core.LexerBuilder;
 import net.sourceforge.aurochs2.core.ParserBuilder;
 import net.sourceforge.aurochs2.core.LexerBuilder.Union;
-import net.sourceforge.aurochs2.core.ParserBuilder.Priority.Associativity;
 
 /**
  * @author codistmonk (creation 2014-08-06)
  */
 public final class ExpressionParser2 implements Serializable {
 	
-	private final Lexer mathLexer;
+	private final Lexer mathLexer = newMathLexer();
 	
-	private final LRParser mathParser;
-	
-	private transient Object object;
-	
-	{
-		final LexerBuilder lexerBuilder = new LexerBuilder();
-		final Union digit = union(range('0', '9'));
-		final Union letter = union(union(range('a', 'z')), union(range('A', 'Z'), disjoin("Σℕℝ")));
-		final Union groupingOperator = disjoin("(){}[]⟨⟩");
-		final Union prefixOperator = disjoin("∀∃¬");
-		final Union infixOperator = disjoin(",=+-*/_^<≤∧∈∩→");
-		final Union postfixOperator = disjoin("ᵀ");
-		final Union operator = merge(groupingOperator, prefixOperator, infixOperator, postfixOperator);
-		
-		lexerBuilder.generate("natural", oneOrMore(digit));
-		lexerBuilder.generate("variable", letter);
-		for (final Object symbol : operator.getSymbols()) {
-			lexerBuilder.generate(symbol.toString(), symbol);
-		}
-		lexerBuilder.generate("string", '\'', "characters", '\'');
-		lexerBuilder.define("characters", union(digit, letter, operator, ' ', sequence('\\', '\'')), "characters");
-		lexerBuilder.define("characters");
-		lexerBuilder.skip(oneOrMore(' '));
-		
-		this.mathLexer = lexerBuilder.newLexer();
-		
-		final ParserBuilder parserBuilder = new ParserBuilder(this.mathLexer);
-		
-		parserBuilder.define("()", "Expression").setListener((rule, data) -> {
-			this.object = data[0];
-			
-			return null;
-		});
-		defineGroupingOperations(parserBuilder, groupingOperator);
-		definePrefixOperations(parserBuilder, prefixOperator);
-		defineInfixOperations(parserBuilder, infixOperator);
-		definePostfixOperations(parserBuilder, postfixOperator);
-		parserBuilder.define("Expression", "-", "Expression").setListener((rule, data) -> $(data));
-		parserBuilder.define("Expression", "Expression", "Expression").setListener((rule, data) -> $(data));
-		parserBuilder.define("Expression", "natural").setListener((rule, data) -> $(data));
-		parserBuilder.define("Expression", "variable").setListener((rule, data) -> $(data));
-		parserBuilder.define("Expression", "string").setListener((rule, data) -> $(data));
-		
-		parserBuilder.resolveConflictWith(block("Expression", "Expression"), block("(", "Expression", ")"));
-//		parserBuilder.resolveConflictWith(block("-", "Expression"), block("(", "Expression", ")"));
-		
-		for (final Object[] pair : groupingPairs(groupingOperator)) {
-			parserBuilder.resolveConflictWith(pair[0].toString(), block("Expression", "-", "Expression"), pair[1].toString());
-		}
-		
-		for (final Object op1 : prefixOperator.getSymbols()) {
-			for (final Object op2 : prefixOperator.getSymbols()) {
-				parserBuilder.resolveConflictWith(block(op1.toString(), "Expression"), block(op2.toString(), "Expression"));
-			}
-		}
-		for (final Object op2 : prefixOperator.getSymbols()) {
-			parserBuilder.resolveConflictWith(block("-", "Expression"), block(op2.toString(), "Expression"));
-		}
-		
-		for (final Object op1 : infixOperator.getSymbols()) {
-			for (final Object op2 : prefixOperator.getSymbols()) {
-				parserBuilder.resolveConflictWith("Expression", op1.toString(), block("Expression", block(op2.toString(), "Expression")));
-			}
-		}
-		
-		for (final Object op1 : prefixOperator.getSymbols()) {
-			for (final Object[] pair : groupingPairs(groupingOperator)) {
-				parserBuilder.resolveConflictWith(block(op1.toString(), "Expression"), block(pair[0].toString(), "Expression", pair[1].toString()));
-			}
-		}
-		for (final Object[] pair : groupingPairs(groupingOperator)) {
-			parserBuilder.resolveConflictWith(block("-", "Expression"), block(pair[0].toString(), "Expression", pair[1].toString()));
-		}
-		
-		for (final Object op1 : infixOperator.getSymbols()) {
-			for (final Object[] pair : groupingPairs(groupingOperator)) {
-				parserBuilder.resolveConflictWith("Expression", op1.toString(), block("Expression", block(pair[0].toString(), "Expression", pair[1].toString())));
-			}
-		}
-		
-		parserBuilder.setPriority(200, NONE, "-", "Expression");
-		for (final Object op : prefixOperator.getSymbols()) {
-			parserBuilder.setPriority(200, LEFT, op.toString(), "Expression");
-		}
-		for (final Object op : infixOperator.getSymbols()) {
-			parserBuilder.setPriority(200, LEFT, "Expression", op.toString(), "Expression");
-		}
-		for (final Object op : postfixOperator.getSymbols()) {
-			parserBuilder.setPriority(300, RIGHT, "Expression", op.toString());
-		}
-		parserBuilder.setPriority(200, LEFT, "Expression", "natural");
-		parserBuilder.setPriority(200, LEFT, "Expression", "variable");
-		parserBuilder.setPriority(200, LEFT, "Expression", "string");
-		parserBuilder.setPriority(200, LEFT, "Expression", "Expression");
-		
-		this.mathParser = parserBuilder.newParser();
-	}
+	private final LRParser mathParser = newMathParser(this.mathLexer);
 	
 	public final <E extends Expression> E parse(final CharSequence input) {
-		if (this.mathParser.parse(this.mathLexer.translate(tokens(input)))) {
-			return $(this.object);
+		final Object[] result = new Object[1];
+		
+		if (this.mathParser.parse(this.mathLexer.translate(tokens(input)), result)) {
+			return $(result[0]);
 		}
 		
 		throw new IllegalArgumentException("Syntax error");
@@ -143,6 +47,20 @@ public final class ExpressionParser2 implements Serializable {
 	 * {@value}.
 	 */
 	private static final long serialVersionUID = -5684274223441131221L;
+	
+	public static final Union DIGIT = union(range('0', '9'));
+	
+	public static final Union LETTER = union(union(range('a', 'z')), union(range('A', 'Z'), disjoin("Σℕℝ")));
+	
+	public static final Union GROUPING_OPERATOR = disjoin("(){}[]⟨⟩");
+	
+	public static final Union PREFIX_OPERATOR = disjoin("∀∃¬");
+	
+	public static final Union INFIX_OPERATOR = disjoin(",=+-*/_^<≤∧∈∩→");
+	
+	public static final Union POSTFIX_OPERATOR = disjoin("ᵀ");
+	
+	public static final Union OPERATOR = merge(GROUPING_OPERATOR, PREFIX_OPERATOR, INFIX_OPERATOR, POSTFIX_OPERATOR);
 	
 	public static final ExpressionParser2 instance = new ExpressionParser2();
 	
@@ -225,6 +143,99 @@ public final class ExpressionParser2 implements Serializable {
 		}
 		
 		return result;
+	}
+	
+	public static final Lexer newMathLexer() {
+		final LexerBuilder resultBuilder = new LexerBuilder();
+		
+		resultBuilder.generate("natural", oneOrMore(DIGIT));
+		resultBuilder.generate("variable", LETTER);
+		for (final Object symbol : OPERATOR.getSymbols()) {
+			resultBuilder.generate(symbol.toString(), symbol);
+		}
+		resultBuilder.generate("string", '\'', "characters", '\'');
+		resultBuilder.define("characters", union(DIGIT, LETTER, OPERATOR, ' ', sequence('\\', '\'')), "characters");
+		resultBuilder.define("characters");
+		resultBuilder.skip(oneOrMore(' '));
+		
+		return resultBuilder.newLexer();
+	}
+	
+	public static final LRParser newMathParser(final Lexer mathLexer) {
+		final ParserBuilder resultBuilder = new ParserBuilder(mathLexer);
+		
+		resultBuilder.define("()", "Expression").setListener((rule, data) -> data[0]);
+		defineGroupingOperations(resultBuilder, GROUPING_OPERATOR);
+		definePrefixOperations(resultBuilder, PREFIX_OPERATOR);
+		defineInfixOperations(resultBuilder, INFIX_OPERATOR);
+		definePostfixOperations(resultBuilder, POSTFIX_OPERATOR);
+		resultBuilder.define("Expression", "-", "Expression").setListener((rule, data) -> $(data));
+		resultBuilder.define("Expression", "Expression", "Expression").setListener((rule, data) -> $(data));
+		resultBuilder.define("Expression", "natural").setListener((rule, data) -> $(data));
+		resultBuilder.define("Expression", "variable").setListener((rule, data) -> $(data));
+		resultBuilder.define("Expression", "string").setListener((rule, data) -> $(data));
+		
+		for (final Object[] pair : groupingPairs(GROUPING_OPERATOR)) {
+			final String left = pair[0].toString();
+			final String right = pair[1].toString();
+			
+			resultBuilder.resolveConflictWith(block("Expression", "Expression"), block(left, "Expression", right));
+			resultBuilder.resolveConflictWith(left, block("Expression", "-", "Expression"), right);
+		}
+		
+		for (final Object op1 : PREFIX_OPERATOR.getSymbols()) {
+			for (final Object op2 : PREFIX_OPERATOR.getSymbols()) {
+				resultBuilder.resolveConflictWith(block(op1.toString(), "Expression"),
+						block(op2.toString(), "Expression"));
+			}
+		}
+		for (final Object op2 : PREFIX_OPERATOR.getSymbols()) {
+			resultBuilder.resolveConflictWith(block("-", "Expression"),
+					block(op2.toString(), "Expression"));
+		}
+		
+		for (final Object op1 : INFIX_OPERATOR.getSymbols()) {
+			for (final Object op2 : PREFIX_OPERATOR.getSymbols()) {
+				resultBuilder.resolveConflictWith("Expression", op1.toString(),
+						block("Expression", block(op2.toString(), "Expression")));
+			}
+		}
+		
+		for (final Object op1 : PREFIX_OPERATOR.getSymbols()) {
+			for (final Object[] pair : groupingPairs(GROUPING_OPERATOR)) {
+				resultBuilder.resolveConflictWith(block(op1.toString(), "Expression"), block(pair[0].toString(), "Expression", pair[1].toString()));
+			}
+		}
+		for (final Object[] pair : groupingPairs(GROUPING_OPERATOR)) {
+			resultBuilder.resolveConflictWith(block("-", "Expression"), block(pair[0].toString(), "Expression", pair[1].toString()));
+		}
+		
+		for (final Object op1 : INFIX_OPERATOR.getSymbols()) {
+			for (final Object[] pair : groupingPairs(GROUPING_OPERATOR)) {
+				resultBuilder.resolveConflictWith("Expression", op1.toString(), block("Expression", block(pair[0].toString(), "Expression", pair[1].toString())));
+			}
+		}
+		
+		for (final Object op : PREFIX_OPERATOR.getSymbols()) {
+			resultBuilder.resolveConflictWith(block("Expression", "Expression"), block(op.toString(), "Expression"));
+		}
+		
+		resultBuilder.setPriority(200, NONE, "-", "Expression");
+		for (final Object op : PREFIX_OPERATOR.getSymbols()) {
+			resultBuilder.setPriority(200, LEFT, op.toString(), "Expression");
+		}
+		for (final Object op : INFIX_OPERATOR.getSymbols()) {
+			resultBuilder.setPriority(200, LEFT, "Expression", op.toString(), "Expression");
+		}
+		for (final Object op : POSTFIX_OPERATOR.getSymbols()) {
+			resultBuilder.setPriority(300, RIGHT, "Expression", op.toString());
+		}
+		resultBuilder.setPriority(200, LEFT, "Expression", "natural");
+		resultBuilder.setPriority(200, LEFT, "Expression", "variable");
+		resultBuilder.setPriority(200, LEFT, "Expression", "string");
+		resultBuilder.setPriority(200, LEFT, "Expression", "Expression");
+		
+		return resultBuilder.newParser();
 	}
 	
 }
