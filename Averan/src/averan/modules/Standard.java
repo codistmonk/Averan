7aeebfc2b37d41194cd73816db4881f10fb454c5
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-27 08:03:23 +0000

    [Averan] Refactoring.

--- Averan/src/averan/modules/Standard.java
+++ Averan/src/averan/modules/Standard.java
@@ -3,16 +3,24 @@ package averan.modules;
 import static averan.core.ExpressionTools.*;
 import static averan.core.Module.ROOT;
 import static averan.core.SessionTools.*;
+import static net.sourceforge.aprog.tools.Tools.cast;
 import static net.sourceforge.aprog.tools.Tools.ignore;
 
 import averan.core.Composite;
 import averan.core.Expression;
 import averan.core.Module;
+import averan.core.Pattern;
 import averan.core.Session;
 import averan.core.Rewriter;
+import averan.core.Module.Bind;
 import averan.core.Module.Symbol;
+import averan.core.Pattern.Any;
+
+import java.util.List;
+import java.util.Map;
 
 import net.sourceforge.aprog.tools.IllegalInstantiationException;
+import net.sourceforge.aprog.tools.Pair;
 
 /**
  * @author codistmonk (creation 2014-08-04)
@@ -115,6 +123,130 @@ public final class Standard {
 		{
 			unifyAndApply(session, RECALL, propositionName);
 		}
+	}
+	
+	public static final void proveUsingBindAndApply(final Expression expression) {
+		proveUsingBindAndApply(session(), expression);
+	}
+	
+	public static final void proveUsingBindAndApply(final Session session, final Expression expression) {
+		Pair<String, Pattern> justificationNameAndPattern = justificationFor(session, expression);
+		
+		if (justificationNameAndPattern == null) {
+			throw new IllegalArgumentException("No justification found for: " + expression);
+		}
+		
+		final String justificationName = justificationNameAndPattern.getFirst();
+		final Pattern pattern = justificationNameAndPattern.getSecond();
+		final Expression justification = session.getProposition(justificationName);
+		final Module module = cast(Module.class, justification);
+		
+		if (module == null) {
+			recall(session, justificationName);
+		} else if (!Standard.ELIMINATION_OF_FALSE.equals(justificationName)) {
+			session.claim(expression);
+			
+			{
+				final Module context = session.getCurrentModule();
+				final Bind bind = context.new Bind(context, justificationName);
+				
+				for (final Map.Entry<Any.Key, Expression> entry : pattern.getBindings().entrySet()) {
+					bind.bind(entry.getKey().toString(), entry.getValue());
+				}
+				
+				bind.execute();
+				
+				for (Module last = cast(Module.class, session.getFact(-1)); last != null && !last.getConditions().isEmpty(); last = cast(Module.class, session.getFact(-1))) {
+					final Expression condition = last.getConditions().get(0);
+					
+					if (expression.equals(condition)) {
+						throw new IllegalStateException("Circularity: " + condition + " needed to prove itself");
+					}
+					
+					proveUsingBindAndApply(session, condition);
+					session.apply(session.getFactName(-2), session.getFactName(-1));
+				}
+			}
+		}
+	}
+	
+	public static final Pair<String, Pattern> justificationFor(final Module context, final Expression target) {
+		Pair<String, Pattern> result = null;
+		
+		for (Module c = context; c != null && result == null; c = c.getParent()) {
+			result = justificationFor(target, c.getFacts(), c.getFactIndices());
+			
+			if (result == null) {
+				result = justificationFor(target, c.getConditions(), c.getConditionIndices());
+			}
+		}
+		
+		return result;
+	}
+	
+	public static final Pair<String, Pattern> justificationFor(final Session session, final Expression target) {
+		Pair<String, Pattern> result = justificationFor(session.getCurrentModule(), target);
+		
+		if (result != null) {
+			return result;
+		}
+		
+		for (final Module module : session.getTrustedModules()) {
+			result = justificationFor(module, target);
+			
+			if (result != null) {
+				return result;
+			}
+		}
+		
+		return null;
+	}
+	
+	public static final Pattern patternFor(final Module context, final Expression expression) {
+		final Rewriter rewriter = new Rewriter();
+		
+		scheduleAnyfyParameters(cast(Module.class, expression), rewriter);
+		
+		for (Module c = context; c != null; c = c.getParent()) {
+			scheduleAnyfyParameters(c, rewriter);
+		}
+		
+		return new Pattern(expression.accept(rewriter));
+	}
+	
+	public static final void scheduleAnyfyParameters(final Module module, final Rewriter rewriter) {
+		if (module == null) {
+			return;
+		}
+		
+		for (final Symbol symbol : module.getParameters()) {
+			rewriter.rewrite(symbol, new Pattern.Any(new Pattern.Any.Key(symbol)));
+		}
+	}
+	
+	private static final Pair<String, Pattern> justificationFor(final Expression target,
+			final List<Expression> propositions, final Map<String, Integer> propositionIndices) {
+		for (final Map.Entry<String, Integer> entry : propositionIndices.entrySet()) {
+			final Expression proposition = propositions.get(entry.getValue());
+			
+			if (target.equals(proposition)) {
+				return new Pair<>(entry.getKey(), new Pattern(target));
+			}
+			
+			Module module = cast(Module.class, proposition);
+			
+			if (module != null) {
+				module = module.canonical();
+				final List<Expression> facts = module.getFacts();
+				final Pattern pattern = patternFor(module, facts.get(facts.size() - 1));
+				
+				if (pattern.equals(target)) {
+					return new Pair<>(entry.getKey(), pattern);
+				}
+			}
+		}
+		
+		return null;
 	}
 	
 }
