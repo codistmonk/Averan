d8757366a6b61dd09f3d2be1b93b3b1ba42f1186
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-08-29 10:34:49 +0000

    [Averan] Refactoring.

--- Averan/demos/averan/demos/Demo2.java
+++ Averan/demos/averan/demos/Demo2.java
@@ -432,20 +432,4 @@ public final class Demo2 {
 		// NOP
 	}
 	
-	/**
-	 * @author codistmonk (creation 2014-08-11)
-	 */
-	public static final class BreakSessionException extends RuntimeException {
-		
-		/**
-		 * {@value}.
-		 */
-		private static final long serialVersionUID = -6958775377557504848L;
-		
-		public static final void breakSession() {
-			throw new BreakSessionException();
-		}
-		
-	}
-	
 }
