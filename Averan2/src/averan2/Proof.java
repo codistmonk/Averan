bcd99fe939ed0d536307d263fc1f7166e2b59c99
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-12-12 08:21:32 +0000

    [Averan2] Updated Proof.

--- Averan2/src/averan2/Proof.java
+++ Averan2/src/averan2/Proof.java
@@ -5,13 +5,23 @@ package averan2;
  */
 public abstract class Proof implements Expression {
 	
-	private final Expression conclusion;
+	private final Module context;
 	
-	protected Proof(final Expression conclusion) {
-		this.conclusion = conclusion;
+	private Expression conclusion;
+	
+	protected Proof(final Module context) {
+		this.context = context;
+	}
+	
+	public final Module getContext() {
+		return this.context;
 	}
 	
 	public final <E extends Expression> E getConclusion() {
+		if (this.conclusion == null) {
+			this.conclusion = this.computeConclusion();
+		}
+		
 		return (E) this.conclusion;
 	}
 	
@@ -20,6 +30,8 @@ public abstract class Proof implements Expression {
 		return visitor.visit(this);
 	}
 	
+	protected abstract Expression computeConclusion();
+	
 	private static final long serialVersionUID = -8318551923110061376L;
 	
 	/**
@@ -27,8 +39,11 @@ public abstract class Proof implements Expression {
 	 */
 	public static final class Admit extends Proof {
 		
-		public Admit(final Expression conclusion) {
-			super(conclusion);
+		private final Expression conclusion;
+		
+		public Admit(final Module context, final Expression conclusion) {
+			super(context);
+			this.conclusion = conclusion;
 		}
 		
 		@Override
@@ -48,9 +63,86 @@ public abstract class Proof implements Expression {
 			return null;
 		}
 		
+		@Override
+		protected final Expression computeConclusion() {
+			return this.conclusion;
+		}
+		
 		private static final long serialVersionUID = 2744181863226646658L;
 		
 		public static final Symbol ADMIT = new Symbol(Module.ROOT, " Admit: ");
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2014-12-12)
+	 */
+	public static final class Bind extends Proof {
+		
+		private final Symbol moduleName;
+		
+		private final Symbol parameter;
+		
+		private final Expression expression;
+		
+		public Bind(final Module context, final Symbol moduleName,
+				final Symbol parameter, final Expression expression) {
+			super(context);
+			this.moduleName = moduleName;
+			this.parameter = parameter;
+			this.expression = expression;
+		}
+		
+		public final Symbol getModuleName() {
+			return this.moduleName;
+		}
+		
+		public final Symbol getParameter() {
+			return this.parameter;
+		}
+		
+		public final Expression getExpression() {
+			return this.expression;
+		}
+		
+		@Override
+		public final int getElementCount() {
+			return 6;
+		}
+		
+		@Override
+		public final <E extends Expression> E getElement(final int index) {
+			switch (index) {
+			case 0:
+				return (E) BIND;
+			case 1:
+				return (E) this.getModuleName();
+			case 2:
+				return (E) DOT;
+			case 3:
+				return (E) this.getParameter();
+			case 4:
+				return (E) USING;
+			case 5:
+				return (E) this.getExpression();
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected final Expression computeConclusion() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		private static final long serialVersionUID = 4047331031987916386L;
+		
+		public static final Symbol BIND = new Symbol(Module.ROOT, " Bind: ");
+		
+		public static final Symbol DOT = new Symbol(Module.ROOT, ".");
+		
+		public static final Symbol USING = new Symbol(Module.ROOT, " Using: ");
 		
 	}
 	
