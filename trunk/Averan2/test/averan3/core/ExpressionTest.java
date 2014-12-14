65951665f0be0a372140f9eb2c7f6c0129ac7428
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-12-14 17:26:32 +0000

    [Averan2][averan3] Started work on new design.

---
+++ trunk/Averan2/test/averan3/core/ExpressionTest.java
@@ -0,0 +1,29 @@
+package averan3.core;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import averan3.core.Expression.Module;
+import net.sourceforge.aprog.tools.Tools;
+
+import org.junit.Test;
+
+/**
+ * @author codistmonk (creation 2014-12-14)
+ */
+public class ExpressionTest {
+	
+	@Test
+	public final void test1() {
+		final Module module1 = new Module();
+		
+		module1.setCondition(module1.new Variable("X"));
+		module1.getFacts().add(new Module(module1, module1.getCondition(), Arrays.asList(module1.getCondition())));
+		
+		Tools.debugPrint(module1);
+		
+		fail("TODO");
+	}
+	
+}
