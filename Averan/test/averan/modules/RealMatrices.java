c33513ba0de7b55377a018da79c512bb911ed563
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-12-15 15:41:03 +0000

    [Averan] Updated RealMatrices.

--- Averan/test/averan/modules/RealMatrices.java
+++ Averan/test/averan/modules/RealMatrices.java
@@ -5,13 +5,13 @@ import static averan.core.SessionTools.*;
 import static averan.io.ExpressionParser.$$;
 import static averan.modules.Reals.real;
 import static averan.modules.Standard.*;
-
 import averan.core.Composite;
 import averan.core.Expression;
 import averan.core.Module;
 import averan.core.Session;
 import averan.core.Module.Symbol;
 import averan.io.SessionExporter;
+import averan.io.SessionScaffold;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -32,68 +32,106 @@ public final class RealMatrices {
 	public static final Module MODULE = new Module(Reals.MODULE, RealMatrices.class.getName());
 	
 	static {
-		pushNewSession(MODULE);
-		
-		try {
-			suppose("definition_of_matrices",
-					$$("∀X,m,n (X∈≀M_(m,n) = ('rowCount'_X = m ∧ 'columnCount'_X = n ∧ ∀i,j (X_(i,j)∈ℝ)))"));
-			suppose("type_of_matrices",
-					$$("∀X ((X∈≀M)=(X∈≀M_(('rowCount'_X),('columnCount'_X))))"));
-			suppose("definition_of_matrix_equality",
-					$$("∀X,Y ((X∈≀M) → ((Y∈≀M) → ((X=Y) = (∀i,j ((X)_(i,j)=(Y_(i,j)))))))"));
-			suppose("definition_of_matrix_scalarization",
-					$$("∀X ((X∈≀M_(1,1)) → (⟨X⟩=X_(1,1)))"));
-			suppose("definition_of_matrix_addition",
-					$$("∀X,Y,i,j (((X+Y)_(i,j) = (X_(i,j))+(Y_(i,j))))"));
-			suppose("type_of_matrix_addition",
-					$$("∀X,Y ((X∈≀M) → ((Y∈≀M) → ((X+Y)∈≀M)))"));
-			suppose("definition_of_matrix_addition_rowCount",
-					$$("∀X,Y ('rowCount'_(X+Y) = 'rowCount'_X)"));
-			suppose("definition_of_matrix_addition_columnCount",
-					$$("∀X,Y ('columnCount'_(X+Y) = 'columnCount'_X)"));
-			suppose("definition_of_matrix_subtraction",
-					$$("∀X,Y,i,j (((X-Y)_(i,j) = (X_(i,j))-(Y_(i,j))))"));
-			suppose("definition_of_matrix_subtraction_rowCount",
-					$$("∀X,Y ('rowCount'_(X-Y) = 'rowCount'_X)"));
-			suppose("definition_of_matrix_subtraction_columnCount",
-					$$("∀X,Y ('columnCount'_(X-Y) = 'columnCount'_X)"));
-			suppose("definition_of_matrix_multiplication",
-					$$("∀X,Y,i,j,k ((XY)_(i,j)=((Σ_(k=0)^(('columnCount'_X)-1)) ((X_(i,k))(Y_(k,j)))))"));
-			suppose("definition_of_matrix_multiplication_rowCount",
-					$$("∀X,Y ('rowCount'_(XY) = 'rowCount'_X)"));
-			suppose("definition_of_matrix_multiplication_columnCount",
-					$$("∀X,Y ('columnCount'_(XY) = 'columnCount'_Y)"));
-			suppose("definition_of_transposition",
-					$$("∀X (∀i,j (Xᵀ_(i,j)=X_(j,i)))"));
-			suppose("definition_of_transposition_rowCount",
-					$$("∀X ('rowCount'_(Xᵀ)='columnCount'_X)"));
-			suppose("definition_of_transposition_columnCount",
-					$$("∀X ('columnCount'_(Xᵀ)='rowCount'_X)"));
+		new SessionScaffold(MODULE) {
 			
-			claim("matrix_element_is_real",
-					$$("∀X ((X∈≀M) → (∀i,j ((X)_(i,j)∈ℝ)))"));
-			{
-				introduce();
-				introduce();
-				introduce();
-				introduce();
+			@Override
+			public final void buildSession() {
+				suppose("definition_of_matrices",
+						$$("∀X,m,n (X∈≀M_(m,n) = ('rowCount'_X = m ∧ 'columnCount'_X = n ∧ ∀i,j (X_(i,j)∈ℝ)))"));
+				suppose("type_of_matrices",
+						$$("∀X ((X∈≀M)=(X∈≀M_(('rowCount'_X),('columnCount'_X))))"));
+				suppose("definition_of_matrix_equality",
+						$$("∀X,Y ((X∈≀M) → ((Y∈≀M) → ((X=Y) = (∀i,j ((X)_(i,j)=(Y_(i,j)))))))"));
+				suppose("definition_of_matrix_scalarization",
+						$$("∀X ((X∈≀M_(1,1)) → (⟨X⟩=X_(1,1)))"));
+				suppose("definition_of_matrix_addition",
+						$$("∀X,Y,i,j (((X+Y)_(i,j) = (X_(i,j))+(Y_(i,j))))"));
+				suppose("type_of_matrix_addition",
+						$$("∀X,Y ((X∈≀M) → ((Y∈≀M) → ((X+Y)∈≀M)))"));
+				suppose("definition_of_matrix_addition_rowCount",
+						$$("∀X,Y ('rowCount'_(X+Y) = 'rowCount'_X)"));
+				suppose("definition_of_matrix_addition_columnCount",
+						$$("∀X,Y ('columnCount'_(X+Y) = 'columnCount'_X)"));
+				suppose("definition_of_matrix_subtraction",
+						$$("∀X,Y,i,j (((X-Y)_(i,j) = (X_(i,j))-(Y_(i,j))))"));
+				suppose("definition_of_matrix_subtraction_rowCount",
+						$$("∀X,Y ('rowCount'_(X-Y) = 'rowCount'_X)"));
+				suppose("definition_of_matrix_subtraction_columnCount",
+						$$("∀X,Y ('columnCount'_(X-Y) = 'columnCount'_X)"));
+				suppose("definition_of_matrix_multiplication",
+						$$("∀X,Y,i,j,k ((XY)_(i,j)=((Σ_(k=0)^(('columnCount'_X)-1)) ((X_(i,k))(Y_(k,j)))))"));
+				suppose("type_of_matrix_multiplication",
+						$$("∀X,Y ((X∈≀M) → ((Y∈≀M) → ((XY)∈≀M)))"));
+				suppose("definition_of_matrix_multiplication_rowCount",
+						$$("∀X,Y ('rowCount'_(XY) = 'rowCount'_X)"));
+				suppose("definition_of_matrix_multiplication_columnCount",
+						$$("∀X,Y ('columnCount'_(XY) = 'columnCount'_Y)"));
+				suppose("definition_of_transposition",
+						$$("∀X (∀i,j (Xᵀ_(i,j)=X_(j,i)))"));
+				suppose("definition_of_transposition_rowCount",
+						$$("∀X ('rowCount'_(Xᵀ)='columnCount'_X)"));
+				suppose("definition_of_transposition_columnCount",
+						$$("∀X ('columnCount'_(Xᵀ)='rowCount'_X)"));
 				
-				final Symbol x = parameter("X");
-				bind("type_of_matrices", x);
-				rewrite(conditionName(-1), factName(-1));
-				bind("definition_of_matrices", x, $("rowCount", "_", x), $("columnCount", "_", x));
-				rewrite(factName(-2), factName(-1));
-				bind(factName(-1));
-				bind(factName(-1), parameter("i"), parameter("j"));
+				claim("matrix_element_is_real",
+						$$("∀X ((X∈≀M) → (∀i,j ((X)_(i,j)∈ℝ)))"));
+				{
+					introduce();
+					introduce();
+					introduce();
+					introduce();
+					
+					final Symbol x = parameter("X");
+					bind("type_of_matrices", x);
+					rewrite(conditionName(-1), factName(-1));
+					bind("definition_of_matrices", x, $("rowCount", "_", x), $("columnCount", "_", x));
+					rewrite(factName(-2), factName(-1));
+					bind(factName(-1));
+					bind(factName(-1), parameter("i"), parameter("j"));
+				}
+				
+				claimAssociativityOfMatrixAddition();
+				claimCommutativityOfMatrixAddition();
+				claim("associativity_of_matrix_multiplication",
+						$$("∀X,Y,Z (X∈≀M → (Y∈≀M → (Z∈≀M → ((X(YZ))=((XY)Z)))))"));
+				{
+					final Symbol x = introduce();
+					final Symbol y = introduce();
+					final Symbol z = introduce();
+					
+					introduce();
+					introduce();
+					introduce();
+					
+					final Expression xy = $(x, y);
+					final Expression yz = $(y, z);
+					final Expression xyZ = $(xy, z);
+					final Expression xYZ = $(x, yz);
+					
+					bind("definition_of_matrix_equality", xyZ, xYZ);
+					
+					autoApplyLastFact();
+					autoApplyLastFact();
+					claim(((Composite) fact(-1)).get(2));
+					{
+						final Symbol i = introduce();
+						final Symbol j = introduce();
+						final Symbol k = session().getCurrentModule().new Symbol("k");
+						final Symbol l = session().getCurrentModule().new Symbol("l");
+						
+						bind("definition_of_matrix_multiplication_columnCount", x, y);
+						bind("definition_of_matrix_multiplication", xy, z, i, j, k);
+						rewrite(factName(-1), factName(-2));
+						bind("definition_of_matrix_multiplication", x, y, i, k, l);
+						bind("definition_of_matrix_multiplication", x, yz, i, j, l);
+						bind("definition_of_matrix_multiplication", y, z, l, j, k);
+					}
+				}
 			}
 			
-			claimAssociativityOfMatrixAddition();
-			claimCommutativityOfMatrixAddition();
+			private static final long serialVersionUID = 8185469030596522271L;
 			
-			new SessionExporter(session()).exportSession();
-		} finally {
-			popSession();
-		}
+		};
 	}
 	
 	public static final void claimCommutativityOfMatrixAddition() {
