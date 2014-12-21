8ed5447178c3701bceb5e4e5f6e88b6959cf80fa
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-12-21 00:19:17 +0000

    [Averan2][averan5] Updated Composite.

--- trunk/Averan2/src/averan5/core/Expression.java
+++ trunk/Averan2/src/averan5/core/Expression.java
@@ -9,6 +9,18 @@ public abstract interface Expression<E extends Expression<?>> extends Container<
 	
 	public abstract <V> V accept(Visitor<V> visitor);
 	
+	public default <I extends Interpretation<E>> I as(final Class<I> interpretationClass) {
+		try {
+			return interpretationClass.getConstructor(this.getClass()).newInstance(this);
+		} catch (final Exception exception) {
+			return null;
+		}
+	}
+	
+	public static final Symbol EQUALS = new Symbol("=");
+	
+	public static final Symbol IMPLIES = new Symbol("->");
+	
 	/**
 	 * @author codistmonk (creation 2014-12-20)
 	 */
@@ -22,8 +34,43 @@ public abstract interface Expression<E extends Expression<?>> extends Container<
 		
 	}
 	
-	public static final Symbol EQUALS = new Symbol("=");
-	
-	public static final Symbol IMPLIES = new Symbol("->");
+	/**
+	 * @author codistmonk (creation 2014-12-20)
+	 */
+	public static abstract interface Interpretation<E extends Expression<?>> extends Serializable {
+		
+		public abstract Composite<E> getComposite();
+		
+		/**
+		 * @author codistmonk (creation 2014)
+		 *
+		 * @param <E>
+		 */
+		public static abstract class Default<E extends Expression<?>> implements Interpretation<E> {
+			
+			private final Composite<E> composite;
+			
+			protected Default(final Composite<E> composite) {
+				this.composite = composite;
+			}
+			
+			@Override
+			public final Composite<E> getComposite() {
+				return this.composite;
+			}
+			
+			private static final long serialVersionUID = 3493628090781849471L;
+			
+			public static final boolean isPair(final Expression<?> expression) {
+				return expression.getElementCount() == 2;
+			}
+			
+			public static final boolean isTriple(final Expression<?> expression) {
+				return expression.getElementCount() == 3;
+			}
+			
+		}
+		
+	}
 	
 }
