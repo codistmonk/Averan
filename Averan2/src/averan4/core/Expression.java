1c7354643983ce3e2b3c37334fb44b31eda0783f
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-12-26 16:28:19 +0000

    [Averan2] Started work on new design.

---
+++ Averan2/src/averan4/core/Expression.java
@@ -0,0 +1,31 @@
+package averan4.core;
+
+import java.io.Serializable;
+
+/**
+ * @author codistmonk (creation 2014-12-20)
+ */
+public abstract interface Expression<E extends Expression<?>> extends Container<E> {
+	
+	public abstract <V> V accept(Visitor<V> visitor);
+	
+	public static final Symbol EQUALS = new Symbol("=");
+	
+	public static final Symbol IMPLIES = new Symbol("->");
+	
+	/**
+	 * @author codistmonk (creation 2014-12-20)
+	 */
+	public static abstract interface Visitor<V> extends Serializable {
+		
+		public abstract V visit(Symbol symbol);
+		
+		public abstract V visit(Variable variable);
+		
+		public abstract V visit(Composite<?> composite);
+		
+		public abstract V visit(Module module);
+		
+	}
+	
+}
