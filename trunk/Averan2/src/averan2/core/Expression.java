44e99695833f83cacd426a70109cd98833ede62f
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2014-12-29 08:20:20 +0000

    [Averan2][averan2] Started work on new variable collection mechanism.

--- trunk/Averan2/src/averan2/core/Expression.java
+++ trunk/Averan2/src/averan2/core/Expression.java
@@ -101,4 +101,150 @@ public abstract interface Expression<E extends Expression<?>> extends Container<
 		
 	}
 	
+	/**
+	 * @author codistmonk (creation 2014-12-29)
+	 */
+	public static final class GatherParameters implements Visitor<Void> {
+		
+		private final Map<Module, Module> moduleContexts = new IdentityHashMap<>();
+		
+		private final Map<Variable, Module> variableContexts = new IdentityHashMap<>();
+		
+		private final List<Module> stack = new ArrayList<>();
+		
+		public final Map<Module, Module> getModuleContexts() {
+			return this.moduleContexts;
+		}
+		
+		public final Map<Variable, Module> getVariableContexts() {
+			return this.variableContexts;
+		}
+		
+		@Override
+		public final Void visit(final Symbol<?> symbol) {
+			return null;
+		}
+		
+		@Override
+		public final Void visit(final Variable variable) {
+			this.getVariableContexts().put(variable,
+					this.getCommonAncestor(this.peek(), this.getVariableContexts().get(variable)));
+			
+			return null;
+		}
+		
+		@Override
+		public final Void visit(final Composite<Expression<?>> composite) {
+			Visitor.visitElementsOf(composite, this);
+			
+			return null;
+		}
+		
+		@Override
+		public final Void visit(final Module module) {
+			final Module context = this.peek();
+			final Module declaredContext = this.getModuleContexts().get(module);
+			final Module actualContext = module.getContext();
+			
+			if (!(nullOrEqual(context, declaredContext) && nullOrEqual(context, actualContext) && nullOrEqual(declaredContext, actualContext))) {
+				throw new IllegalStateException();
+			}
+			
+			this.getModuleContexts().put(module, select(context, declaredContext, actualContext));
+			
+			this.push(module);
+			
+			Visitor.visitElementsOf(module, this);
+			
+			this.pop();
+			
+			return null;
+		}
+		
+		@Override
+		public final Void visit(final Substitution substitution) {
+			Visitor.visitElementsOf(substitution, this);
+			
+			return null;
+		}
+		
+		@Override
+		public final Void visit(final Equality equality) {
+			Visitor.visitElementsOf(equality, this);
+			
+			return null;
+		}
+		
+		private final void push(final Module module) {
+			this.stack.add(module);
+		}
+		
+		private final Module peek() {
+			return this.stack.isEmpty() ? null : this.stack.get(this.stack.size() - 1);
+		}
+		
+		private final void pop() {
+			this.stack.remove(this.stack.size() - 1);
+		}
+		
+		private final Module getCommonAncestor(final Module module1, final Module module2) {
+			if (module1 == module2) {
+				return module1;
+			}
+			
+			Module m1 = module1;
+			Module m2 = module2;
+			int depth1 = this.getDepth(module1);
+			int depth2 = this.getDepth(module2);
+			
+			while (depth1 < depth2) {
+				m2 = this.getModuleContexts().get(m2);
+				--depth2;
+			}
+			
+			while (depth2 < depth1) {
+				m1 = this.getModuleContexts().get(m1);
+				--depth1;
+			}
+			
+			if (depth1 != depth2) {
+				throw new IllegalStateException();
+			}
+			
+			while (m1 != null && m2 != null && m1 != m2) {
+				m1 = this.getModuleContexts().get(m1);
+				m2 = this.getModuleContexts().get(m2);
+			}
+			
+			if (m1 != m2) {
+				throw new IllegalStateException();
+			}
+			
+			return m1;
+		}
+		
+		private final int getDepth(final Module module) {
+			final Module context = this.getModuleContexts().get(module);
+			
+			return 1 + (context == null ? 0 : this.getDepth(context));
+		}
+		
+		private static final long serialVersionUID = -1826106345804095658L;
+		
+		public static final boolean nullOrEqual(final Object object1, final Object object2) {
+			return object1 == null || object2 == null || object1 == object2;
+		}
+		
+		public static final <T> T select(final T... objects) {
+			for (final T object : objects) {
+				if (object != null) {
+					return object;
+				}
+			}
+			
+			throw new IllegalStateException();
+		}
+		
+	}
+	
 }
