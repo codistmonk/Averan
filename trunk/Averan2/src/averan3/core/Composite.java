9240bb13a6b4476d688cdfb5e38f51971957a096
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2015-01-04 08:04:49 +0000

    [Averan2] Started work on new design.

---
+++ trunk/Averan2/src/averan3/core/Composite.java
@@ -0,0 +1,103 @@
+package averan3.core;
+
+import static net.sourceforge.aprog.tools.Tools.cast;
+import static net.sourceforge.aprog.tools.Tools.join;
+import averan.common.Container;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author codistmonk (creation 2015-01-04)
+ */
+final class Composite<E extends Expression<?>> implements Expression<E> {
+	
+	private final Composite<?> parent;
+	
+	private final Composite<Variable> parameters;
+	
+	private final List<E> elements;
+	
+	public Composite(final Composite<?> parent) {
+		this.parent = parent;
+		this.parameters = new Composite<>(this);
+		this.elements = new ArrayList<>();
+	}
+	
+	public final Composite<?> getParent() {
+		return this.parent;
+	}
+	
+	public final Composite<Variable> getParameters() {
+		return this.parameters;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final Composite<E> add(final E element) {
+		final Composite<?> composite = cast(Composite.class, element);
+		
+		if (composite == null || composite.getParent() == this) {
+			this.elements.add((E) element);
+		} else {
+			final Composite<Expression<?>> newComposite = new Composite<>(this);
+			
+			composite.getParameters().forEach(newComposite.getParameters()::add);
+			composite.forEach(newComposite::add);
+			
+			this.elements.add((E) newComposite);
+		}
+		
+		return this;
+	}
+	
+	@Override
+	public final int size() {
+		return this.elements.size();
+	}
+	
+	@Override
+	public final E get(final int index) {
+		return 0 <= index && index < this.size() ? this.elements.get(index) : null;
+	}
+	
+	@Override
+	public final <V> V accept(final Visitor<V> visitor) {
+		return visitor.visit(this);
+	}
+	
+	@Override
+	public final int hashCode() {
+		return this.elements.hashCode();
+	}
+	
+	@Override
+	public final boolean equals(final Object object) {
+		if (this == object) {
+			return true;
+		}
+		
+		if (object instanceof Variable) {
+			return object.equals(this);
+		}
+		
+		final Composite<?> that = cast(this.getClass(), object);
+		
+		return that != null && this.elements.equals(that.elements);
+	}
+	
+	@Override
+	public final String toString() {
+		return formatParameters(this.getParameters()) + this.elements.toString();
+	}
+	
+	private static final long serialVersionUID = -7248608001486237448L;
+	
+	public static final Symbol<String> IMPLIES = new Symbol<>("->");
+	
+	public static final Symbol<String> EQUALS = new Symbol<>("=");
+	
+	public static final String formatParameters(final Container<Variable> parameters) {
+		return parameters.isEmpty() ? "" : ("∀" + join(",", parameters.toArray()) + " ");
+	}
+	
+}
---
+++ trunk/Averan2/test/averan3/core/ExpressionTest.java
@@ -0,0 +1,164 @@
+package averan3.core;
+
+import static averan3.core.Composite.IMPLIES;
+import static net.sourceforge.aprog.tools.Tools.cast;
+import static org.junit.Assert.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+
+import net.sourceforge.aprog.tools.Tools;
+
+import org.junit.Test;
+
+/**
+ * @author codistmonk (creation 2015-01-04)
+ */
+public final class ExpressionTest {
+	
+	@Test
+	public final void test() {
+		fail("Not yet implemented");
+	}
+	
+	public static abstract interface Proof extends Serializable {
+		
+		public abstract Deduction getParent();
+		
+		public abstract void apply();
+		
+	}
+	
+	/**
+	 * @author codistmonk (creation 2015-01-04)
+	 */
+	public static final class Deduction implements Proof {
+		
+		private final Deduction parent;
+		
+		private final String name;
+		
+		private final Composite<Expression<?>> module;
+		
+		private final List<Proof> proofs;
+		
+		private final Collection<Symbol<String>> protoparameters;
+		
+		private Expression<?> goal;
+		
+		public Deduction(final Deduction parent, final String name, final Composite<Expression<?>> module, final Expression<?> goal) {
+			this.parent = parent;
+			this.name = name;
+			this.module = module;
+			this.proofs = new ArrayList<>();
+			this.protoparameters = new LinkedHashSet<>();
+			this.goal = goal;
+			// primitive module operations: parametrize, suppose, apply, substitute
+			// primitive deduction operations: introduce, deduce, conclude
+			// standard tactics: recall, bind, rewrite, rewriteRight, autoDeduce
+		}
+		
+		public final Expression<?> introduce(final String parameterName) {
+			final Composite<?> goal = cast(Composite.class, this.getGoal());
+			
+			if (goal != null && !goal.getParameters().isEmpty()) {
+				final Variable parameter = goal.getParameters().get(0);
+				final Symbol<String> result = new Symbol<String>(parameterName != null ? parameterName : parameter.getName());
+				
+				if (!this.protoparameters.add(result)) {
+					throw new IllegalArgumentException();
+				}
+				
+				this.goal = this.goal.accept(new Expression.Substitution().bind(new Variable(parameter.getName(), parameter), result));
+				
+				return result;
+			}
+			
+			return null;
+		}
+		
+		@Override
+		public final Deduction getParent() {
+			return this.parent;
+		}
+		
+		@Override
+		public final void apply() {
+			// TODO Auto-generated method stub
+		}
+		
+		public final String getName() {
+			return this.name;
+		}
+		
+		public final Composite<Expression<?>> getModule() {
+			return this.module;
+		}
+		
+		public final Expression<?> getGoal() {
+			return this.goal;
+		}
+		
+		private static final long serialVersionUID = -4622604986554143041L;
+		
+		/**
+		 * @author codistmonk (creation 2015-01-04)
+		 */
+		public final class Supposition implements Proof {
+			
+			private final String propositionName;
+			
+			private final Expression<?> proposition;
+			
+			public Supposition(final String propositionName, final Expression<?> proposition) {
+				this.propositionName = propositionName;
+				this.proposition = proposition;
+			}
+			
+			public final String getPropositionName() {
+				return this.propositionName;
+			}
+			
+			public final Expression<?> getProposition() {
+				return this.proposition;
+			}
+			
+			@Override
+			public final Deduction getParent() {
+				return Deduction.this;
+			}
+			
+			@Override
+			public final void apply() {
+				final Composite<Expression<?>> module = this.getParent().getModule();
+				
+				switch (module.size()) {
+				case 0:
+					module.add(this.getProposition());
+					break;
+				case 1:
+					module.add(IMPLIES).add(this.getProposition());
+					break;
+				case 3:
+					if (IMPLIES.equals(module.get(1))) {
+						// XXX what now?
+						throw new RuntimeException("TODO");
+//						break;
+					}
+				default:
+					throw new IllegalStateException();
+				}
+				
+				this.getParent().proofs.add(this);
+			}
+			
+			private static final long serialVersionUID = -2449310594857640213L;
+			
+		}
+		
+	}
+	
+}
