043331ad5f40ad5de6deb1ef647279fcc7abd10c
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2015-01-04 18:34:05 +0000

    [Averan2][averan3] Refactoring.

---
+++ trunk/Averan2/test/averan3/core/ProofTest.java
@@ -0,0 +1,17 @@
+package averan3.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author codistmonk (creation 2015-01-04)
+ */
+public final class ProofTest {
+	
+	@Test
+	public final void test() {
+		fail("Not yet implemented");
+	}
+	
+}
