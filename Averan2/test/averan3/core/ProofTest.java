5ffdde01c19e43bad1cb6f8e1af18f25a202ac06
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2015-01-05 10:34:33 +0000

    [Averan2][averan3] Updated ProofTest.

--- Averan2/test/averan3/core/ProofTest.java
+++ Averan2/test/averan3/core/ProofTest.java
@@ -1,7 +1,13 @@
 package averan3.core;
 
+import static java.util.Collections.nCopies;
 import static net.sourceforge.aprog.tools.Tools.getThisMethodName;
+import static net.sourceforge.aprog.tools.Tools.join;
 import static org.junit.Assert.*;
+
+import java.io.PrintStream;
+import java.util.Collections;
+
 import net.sourceforge.aprog.tools.Tools;
 
 import org.junit.Test;
@@ -15,9 +21,43 @@ public final class ProofTest {
 	
 	@Test
 	public final void test() {
-		final Deduction deduction = new Deduction(null, getThisMethodName(), null, null);
+		final Deduction deduction = new Deduction(null, getThisMethodName(), null);
 		
-		fail("Not yet implemented");
+		try {
+			final Symbol<String> x = (Symbol<String>) deduction.introduce("X");
+			
+			deduction.new Supposition(null, x).apply();
+		} finally {
+			export(deduction);
+		}
+	}
+	
+	public static final void export(final Deduction deduction) {
+		export(deduction, 0, System.out);
+	}
+	
+	public static final void export(final Deduction deduction, final int level, final PrintStream out) {
+		final String indent = join("", nCopies(level, '	'));
+		final String indent1 = indent + '	';
+		final String indent2 = indent1 + '	';
+		
+		out.println(indent + "Deduce (" + deduction.getName() + ")");
+		
+		if (!deduction.getProtoparameters().isEmpty()) {
+			out.println(indent2 + '∀' + join(",", deduction.getProtoparameters()));
+		}
+		
+		for (final Proof proof : deduction.getProofs()) {
+			out.println(indent1 + "(" + proof.getName() + ")");
+			out.println(indent1 + proof);
+			out.println(indent2 + proof.getProposition());
+		}
+		
+		if (deduction.getGoal() != null) {
+			out.println(indent + "Goal: " + deduction.getGoal());
+		}
+		
+		out.println(indent + ".");
 	}
 	
 }
