5bbb57f7d41f330456262214563586d570e0818c
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2015-01-06 18:19:07 +0000

    [Averan2][averan4] Started work on new design (minor variation from averan3).

---
+++ Averan2/src/averan4/core/Composite.java
@@ -0,0 +1,179 @@
+package averan4.core;
+
+import static net.sourceforge.aprog.tools.Tools.cast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author codistmonk (creation 2015-01-04)
+ */
+final class Composite<E extends Expression<?>> implements Expression<E> {
+	
+	private final List<E> elements;
+	
+	public Composite() {
+		this.elements = new ArrayList<>();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final <F extends E> F getKey() {
+		return this.size() == 3 && EQUALS.equals(this.get(1)) ? (F) this.get(0) : null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final <F extends E> F getValue() {
+		return this.getKey() != null ? (F) this.get(2) : null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final <F extends E> F getCondition() {
+		return this.size() == 3 && IMPLIES.equals(this.get(1)) ? (F) this.get(0) : null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final <F extends E> F getConclusion() {
+		return this.getCondition() != null ? (F) this.get(2) : null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final <F extends E> F getHead() {
+		return this.getTail() != null ? (F) this.get(0) : null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final Composite<Expression<?>> getTail() {
+		return this.size() == 2 ? cast(Composite.class, this.get(1)) : null;
+	}
+	
+	public final Composite<Expression<?>> getParameters() {
+		if (this.size() == 2) {
+			@SuppressWarnings("unchecked")
+			final Composite<Expression<?>> candidate = cast(Composite.class, this.get(0));
+			
+			if (candidate != null && (2 <= candidate.size() && FORALL.equals(candidate.get(0)))) {
+				return candidate;
+			}
+		}
+		
+		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final <F extends Expression<?>> F getContents() {
+		return (F) (this.getParameters() != null ? this.get(1) : this);
+	}
+	
+	public final int getListSize() {
+		return this.isEmpty() ? 0 : 1 + this.getTail().getListSize();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final E getListElement(final int index) {
+		if (index < 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		return index == 0 ? this.getHead() : (E) this.getTail().getListElement(index - 1);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public final Composite<E> append(final E element) {
+		Composite<Expression<?>> end = (Composite<Expression<?>>) this;
+		
+		while (!end.isEmpty()) {
+			end = end.getTail();
+		}
+		
+		end.add(element).add(new Composite<>());
+		
+		return this;
+	}
+	
+	public final Composite<E> add(final E element) {
+		this.elements.add(element);
+		
+		return this;
+	}
+	
+	public final E removeLast() {
+		return this.elements.remove(this.size() - 1);
+	}
+	
+	@Override
+	public final int size() {
+		return this.elements.size();
+	}
+	
+	@Override
+	public final E get(final int index) {
+		return 0 <= index && index < this.size() ? this.elements.get(index) : null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public final <V> V accept(final Visitor<V> visitor) {
+		return visitor.visit((Composite<Expression<?>>) this);
+	}
+	
+	@Override
+	public final boolean implies(final Expression<?> expression) {
+		final Composite<?> that = cast(this.getClass(), expression);
+		
+		if (that == null) {
+			return false;
+		}
+		
+		final int thisSize = this.size();
+		final int thatSize = that.size();
+		
+		full_implication:
+		if (thisSize == thatSize) {
+			for (int i = 0; i < thisSize; ++i) {
+				if (!this.get(i).implies(that.get(i))) {
+					break full_implication;
+				}
+			}
+		}
+		
+		if (this.getParameters() != null) {
+			return this.getContents().implies(expression);
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public final int hashCode() {
+		return this.elements.hashCode();
+	}
+	
+	@Override
+	public final boolean equals(final Object object) {
+		if (this == object) {
+			return true;
+		}
+		
+		if (object instanceof Variable) {
+			return object.equals(this);
+		}
+		
+		final Composite<?> that = cast(this.getClass(), object);
+		
+		return that != null && this.elements.equals(that.elements);
+	}
+	
+	@Override
+	public final String toString() {
+		return this.elements.toString();
+	}
+	
+	private static final long serialVersionUID = -7248608001486237448L;
+	
+	public static final Symbol<String> FORALL = new Symbol<>("∀");
+	
+	public static final Symbol<String> IMPLIES = new Symbol<>("→");
+	
+	public static final Symbol<String> EQUALS = new Symbol<>("=");
+	
+}
---
+++ Averan2/test/averan4/core/ExpressionTest.java
@@ -0,0 +1,94 @@
+package averan4.core;
+
+import static averan4.core.Composite.FORALL;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author codistmonk (creation 2015-01-05)
+ */
+public final class ExpressionTest {
+	
+	@Test
+	public final void testBind1() {
+		final Expression<?> expression = new Symbol<>("test");
+		
+		assertSame(expression, expression.accept(Variable.BIND));
+	}
+	
+	@Test
+	public final void testBind2() {
+		{
+			final Expression<?> expression = new Variable("test");
+			
+			assertSame(expression, expression.accept(Variable.BIND));
+		}
+		{
+			final Expression<?> expression = new Variable("test");
+			
+			expression.equals(new Symbol<>("test"));
+			
+			final Expression<?> actual = expression.accept(Variable.BIND);
+			
+			assertNotSame(expression, actual);
+			assertEquals(new Symbol<>("test"), actual);
+		}
+	}
+	
+	@Test
+	public final void testBind3() {
+		{
+			final Expression<?> expression = new Composite<>();
+			
+			assertSame(expression, expression.accept(Variable.BIND));
+		}
+		{
+			final Expression<?> expression = new Composite<>().add(new Symbol<>("test")).add(new Variable("test"));
+			
+			assertSame(expression, expression.accept(Variable.BIND));
+		}
+		{
+			final Variable variable = new Variable("test");
+			final Expression<?> expression = new Composite<>().add(new Symbol<>("test")).add(variable);
+			
+			variable.equals(new Symbol<>("test"));
+			
+			final Expression<?> actual = expression.accept(Variable.BIND);
+			
+			assertNotSame(expression, actual);
+			assertEquals(new Composite<>().add(new Symbol<>("test")).add(new Symbol<>("test")), actual);
+		}
+		{
+			final Variable variable = new Variable("test");
+			final Expression<?> expression = new Composite<>().add(new Composite<>().append(FORALL).append(variable)).add(variable);
+			
+			variable.equals(new Symbol<>("test"));
+			
+			final Expression<?> actual = expression.accept(Variable.BIND);
+			
+			assertNotSame(expression, actual);
+			assertEquals(new Symbol<>("test"), actual);
+		}
+	}
+	
+	@Test
+	public final void testSubstitute1() {
+		{
+			final Variable variable = new Variable("test");
+			final Expression<?> expression = new Composite<>().add(new Composite<>().add(new Composite<>().append(FORALL).append(variable)).add(variable));
+			final Expression<?> actual = expression.accept(new Expression.Substitution().bind(variable, new Symbol<>("test")));
+			
+			assertNotSame(expression, actual);
+			assertEquals(new Composite<>().add(new Symbol<>("test")), actual);
+		}
+		{
+			final Expression<?> expression = new Composite<>().add(new Symbol<>("a")).add(new Composite<>().add(new Symbol<>("a")).add(new Symbol<>("a")));
+			final Expression<?> actual = expression.accept(new Expression.Substitution().bind(new Symbol<>("a"), new Symbol<>("b")).at(0, 2));
+			
+			assertNotSame(expression, actual);
+			assertEquals(new Composite<>().add(new Symbol<>("b")).add(new Composite<>().add(new Symbol<>("a")).add(new Symbol<>("b"))), actual);
+		}
+	}
+	
+}
