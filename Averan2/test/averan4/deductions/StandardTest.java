e1c43afcc01f3f53ce8fb9250f83a82eb0364f2b
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2015-01-07 10:39:49 +0000

    [Averan2][averan3] Updated tests.

--- Averan2/test/averan4/deductions/StandardTest.java
+++ Averan2/test/averan4/deductions/StandardTest.java
@@ -1,8 +1,13 @@
 package averan4.deductions;
 
-import static org.junit.Assert.*;
+import static averan4.core.Composite.EQUALS;
+import static averan4.core.Session.*;
+import static averan4.deductions.Standard.*;
+import static net.sourceforge.aprog.tools.Tools.getThisMethodName;
 
 import org.junit.Test;
+
+import averan4.io.ConsoleOutput;
 
 /**
  * @author codistmonk (creation 2015-01-07)
@@ -10,8 +15,32 @@ import org.junit.Test;
 public final class StandardTest {
 	
 	@Test
-	public final void test() {
-		assertNotNull(Standard.DEDUCTION);
+	public final void test1() {
+		final String deductionName = this.getClass().getName() + "." + getThisMethodName();
+		
+		build(deductionName, () -> {
+			include(Standard.DEDUCTION);
+			
+			bind1("test1", "recall", $("toto"));
+			
+			bind1("test2", "symmetry_of_equality", $("toto"));
+			
+			deduce("test3");
+			{
+				suppose($("a", EQUALS, "b"));
+				suppose($("a"));
+				rewrite1(name(-1), name(-2));
+				conclude();
+			}
+			
+			deduce("test4");
+			{
+				suppose($("a", EQUALS, "b"));
+				suppose($("b"));
+				rewriteRight(name(-1), name(-2));
+				conclude();
+			}
+		}, new ConsoleOutput());
 	}
 	
 }
