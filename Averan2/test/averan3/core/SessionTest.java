613916f59c49ecab822de010439eb369edafa502
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2015-01-07 15:58:10 +0000

    [Averan2][averan3] Updated SessionTest.

--- Averan2/test/averan3/core/SessionTest.java
+++ Averan2/test/averan3/core/SessionTest.java
@@ -7,6 +7,7 @@ import static net.sourceforge.aprog.tools.Tools.array;
 import static net.sourceforge.aprog.tools.Tools.cast;
 import static net.sourceforge.aprog.tools.Tools.getThisMethodName;
 import averan3.core.Proof.Deduction;
+import averan3.deductions.Standard;
 import averan3.io.ConsoleOutput;
 
 import java.io.Serializable;
@@ -51,6 +52,8 @@ public final class SessionTest {
 		final String deductionName = this.getClass().getName() + "." + getThisMethodName();
 		
 		build(deductionName, () -> {
+			include(Standard.DEDUCTION);
+			
 			deduce($($("a", IMPLIES, "b"), IMPLIES, $($("b", IMPLIES, "c"), IMPLIES, $("a", IMPLIES, "c"))));
 			{
 				intros();
@@ -76,10 +79,6 @@ public final class SessionTest {
 	
 	public static final AtomicInteger autoDeduceDepth = new AtomicInteger(4); 
 	
-	public static final boolean autoDeduce() {
-		return autoDeduce(null, goal(), autoDeduceDepth.get());
-	}
-	
 	public static final void check(final boolean ok) {
 		check(ok, "");
 	}
@@ -88,6 +87,34 @@ public final class SessionTest {
 		if (!ok) {
 			throw new RuntimeException(message);
 		}
+	}
+	
+	public static final boolean autoDeduce() {
+		return autoDeduce(null, goal());
+	}
+	
+	public static final boolean autoDeduce(final String propositionName) {
+		return autoDeduce(propositionName, goal());
+	}
+	
+	public static final boolean autoDeduce(final Expression<?> goal) {
+		return autoDeduce(null, goal);
+	}
+	
+	public static final boolean autoDeduce(final String propositionName, final Expression<?> goal) {
+		return autoDeduce(propositionName, goal, autoDeduceDepth.get());
+	}
+	
+	public static final boolean autoDeduce(final int depth) {
+		return autoDeduce(null, goal(), depth);
+	}
+	
+	public static final boolean autoDeduce(final String propositionName, final int depth) {
+		return autoDeduce(propositionName, goal(), depth);
+	}
+	
+	public static final boolean autoDeduce(final Expression<?> goal, final int depth) {
+		return autoDeduce(null, goal, depth);
 	}
 	
 	public static final boolean autoDeduce(final String propositionName, final Expression<?> goal, final int depth) {
@@ -99,12 +126,39 @@ public final class SessionTest {
 		{
 			intros();
 			
-			final List<Justification> justifications = justify(goal);
+			int d = depth;
 			
-			Tools.debugPrint(justifications);
-			
-			for (final Justification justification : justifications) {
+			use_a_justification:
+			while (true) {
+				for (final Justification justification : justify(goal)) {
+					final Justification.Step step = justification.getSteps()[0];
+					
+					if (step instanceof Justification.Recall) {
+						apply("recall", justification.getPropositionName());
+						break use_a_justification;
+					}
+					
+					if (step instanceof Justification.Apply) {
+						if (autoDeduce(((Justification.Apply) step).getCondition(), depth - 1)) {
+							apply(justification.getPropositionName(), name(-1));
+							
+							if (1 < justification.getSteps().length) {
+								break;
+							} else {
+								break use_a_justification;
+							}
+						}
+					}
+					
+					if (step instanceof Justification.Bind) {
+						Tools.debugError("TODO"); // TODO
+					}
+				}
 				
+				if (--d <= 0) {
+					cancel();
+					return false;
+				}
 			}
 			
 			conclude();
@@ -124,7 +178,7 @@ public final class SessionTest {
 			for (int i = proofs.size() - 1; 0 <= i; --i) {
 				final Proof proof = proofs.get(i);
 				final Expression<?> proposition = proof.getProposition();
-				final Object[] steps = findSteps(proposition, goal);
+				final Justification.Step[] steps = findSteps(proposition, goal);
 				
 				if (0 < steps.length) {
 					result.add(new Justification(proof.getPropositionName(), steps, proposition.accept(Variable.BIND)));
@@ -137,9 +191,9 @@ public final class SessionTest {
 		return result;
 	}
 	
-	public static final Object[] findSteps(final Expression<?> proposition, final Expression<?> goal, final Object... steps) {
+	public static final Justification.Step[] findSteps(final Expression<?> proposition, final Expression<?> goal, final Justification.Step... steps) {
 		if (proposition.accept(Variable.RESET).equals(goal.accept(Variable.RESET))) {
-			return append(steps, EQUALS);
+			return append(steps, new Justification.Recall());
 		}
 		
 		proposition.accept(Variable.RESET);
@@ -154,11 +208,13 @@ public final class SessionTest {
 		final Composite<Expression<?>> parameters = composite.getParameters();
 		
 		if (parameters != null && parameters.isList()) {
-			return findSteps(composite.getContents(), goal, append(steps, FORALL));
+			return findSteps(composite.getContents(), goal, append(steps,
+					new Justification.Bind(parameters)));
 		}
 		
 		if (composite.getCondition() != null) {
-			return findSteps(composite.getConclusion(), goal, append(steps, IMPLIES));
+			return findSteps(composite.getConclusion(), goal, append(steps,
+					new Justification.Apply(composite.getCondition().accept(Variable.BIND))));
 		}
 		
 		return array();
@@ -171,11 +227,11 @@ public final class SessionTest {
 		
 		private final String propositionName;
 		
-		private final Object[] steps;
+		private final Step[] steps;
 		
 		private final Expression<?> boundProposition;
 		
-		public Justification(final String propositionName, final Object[] steps,
+		public Justification(final String propositionName, final Step[] steps,
 				final Expression<?> boundProposition) {
 			this.propositionName = propositionName;
 			this.steps = steps;
@@ -186,7 +242,7 @@ public final class SessionTest {
 			return this.propositionName;
 		}
 		
-		public final Object[] getSteps() {
+		public final Step[] getSteps() {
 			return this.steps;
 		}
 		
@@ -200,6 +256,60 @@ public final class SessionTest {
 		}
 		
 		private static final long serialVersionUID = -3897122482315195936L;
+		
+		/**
+		 * @author codistmonk (creation 2015-01-07)
+		 */
+		public static abstract interface Step extends Serializable {
+			// NOP
+		}
+		
+		/**
+		 * @author codistmonk (creation 2015-01-07)
+		 */
+		public static final class Recall implements Step {
+			
+			private static final long serialVersionUID = -391482341455285510L;
+			
+		}
+		
+		/**
+		 * @author codistmonk (creation 2015-01-07)
+		 */
+		public static final class Apply implements Step {
+			
+			private final Expression<?> condition;
+			
+			public Apply(final Expression<?> condition) {
+				this.condition = condition;
+			}
+			
+			public final Expression<?> getCondition() {
+				return this.condition;
+			}
+			
+			private static final long serialVersionUID = -882415070219823810L;
+			
+		}
+		
+		/**
+		 * @author codistmonk (creation 2015-01-07)
+		 */
+		public static final class Bind implements Step {
+			
+			private final Composite<Expression<?>> parameters;
+			
+			public Bind(final Composite<Expression<?>> parameters) {
+				this.parameters = parameters;
+			}
+			
+			public final Composite<Expression<?>> getParameters() {
+				return this.parameters;
+			}
+			
+			private static final long serialVersionUID = -2178774924606129974L;
+			
+		}
 		
 	}
 	
