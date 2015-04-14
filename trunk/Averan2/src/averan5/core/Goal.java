ab39ad153898fa6d8781c79e4ff005d027aedc67
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2015-04-14 17:14:02 +0000

    [Averan2][averan5] Started work on simplified design.

---
+++ trunk/Averan2/src/averan5/core/Goal.java
@@ -0,0 +1,85 @@
+package averan5.core;
+
+import static averan5.core.AveranTools.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author codistmonk (creation 2015-04-12)
+ */
+public final class Goal implements Serializable {
+	
+	private final List<Object> initialProposition;
+	
+	private List<Object> proposition;
+	
+	private final Deduction deduction;
+	
+	public Goal(final List<Object> proposition, final Deduction context, final String deductionName) {
+		this.initialProposition = proposition;
+		this.proposition = new ArrayList<>(proposition);
+		this.deduction = push(new Deduction(context, deductionName));
+	}
+	
+	public final List<Object> getInitialProposition() {
+		return this.initialProposition;
+	}
+	
+	public final List<Object> getProposition() {
+		return this.proposition;
+	}
+	
+	public final Deduction getDeduction() {
+		return this.deduction;
+	}
+	
+	public final List<Object> introduce() {
+		List<Object> result = null;
+		
+		if (isBlock(this.getProposition())) {
+			result = variable(quantification(this.getProposition()));
+			this.proposition = scope(this.getProposition());
+			
+			this.getDeduction().forall(result);
+		} else if (isRule(this.getProposition())) {
+			result = condition(this.getProposition());
+			this.proposition = conclusion(this.getProposition());
+			
+			this.getDeduction().suppose(this.getDeduction().newPropositionName(), result);
+		}
+		
+		return result;
+	}
+	
+	public final void intros() {
+		while (this.introduce() != null) {
+			// NOP
+		}
+	}
+	
+	public final void conclude() {
+		if (pop() != this.getDeduction()) {
+			throw new IllegalStateException();
+		}
+		
+		final List<Object> provedProposition = this.getDeduction().getProvedProposition();
+		
+		checkState(areEqual(this.getInitialProposition(), provedProposition),
+				"Expected: " + this.getInitialProposition() + " but was: " + provedProposition);
+		
+		deduction().conclude(this.getDeduction());
+	}
+	
+	private static final long serialVersionUID = -6412523746037749196L;
+	
+	public static final Goal deduce(final List<Object> proposition) {
+		return deduce(newName(), proposition);
+	}
+	
+	public static final Goal deduce(final String propositionName, final List<Object> proposition) {
+		return new Goal(proposition, deduction(), propositionName);
+	}
+	
+}
---
+++ trunk/Averan2/src/averan5/core/Substitution.java
@@ -0,0 +1,77 @@
+package averan5.core;
+
+import static averan4.core.AveranTools.*;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+
+/**
+ * @author codistmonk (creation 2015-04-11)
+ */
+public final class Substitution extends Proof.Abstract {
+	
+	private final List<Object> target;
+	
+	private final Map<List<Object>, List<Object>> equalities;
+	
+	private final Collection<Integer> indices;
+	
+	public Substitution(final String provedPropositionName, final List<Object> target,
+			final Map<List<Object>, List<Object>> equalities, final Collection<Integer> indices) {
+		super(provedPropositionName, Arrays.asList("By substituting in", target, "using", equalities, "at", null));
+		this.target = target;
+		this.equalities = equalities;
+		this.indices = indices instanceof TreeSet ? indices : new TreeSet<>(indices);
+		
+		this.getMessage().set(5, this.getIndices());
+	}
+	
+	public final List<Object> getTarget() {
+		return this.target;
+	}
+	
+	public final Map<List<Object>, List<Object>> getEqualities() {
+		return this.equalities;
+	}
+	
+	public final Collection<Integer> getIndices() {
+		return this.indices;
+	}
+	
+	@Override
+	public final List<Object> getProvedPropositionFor(final Deduction context) {
+		final List<Object> substitution = $(this.getTarget(),
+				GIVEN, join(AND, iterable(
+						this.getEqualities().entrySet().stream().map(e -> $equality(e.getKey(), e.getValue())))),
+				AT, new ArrayList<>(this.getIndices()));
+		final List<Object> substituted = substituteIn(this.getTarget(), this.getEqualities(), this.getIndices());
+		
+		return $equality(substitution, substituted);
+	}
+	
+	private static final long serialVersionUID = -5039934017175763847L;
+	
+	public static final List<Object> substituteIn(final List<Object> target,
+			final Map<List<Object>, List<Object>> equalities, final Collection<Integer> indices) {
+		return substituteIn(target, equalities, indices, new int[] { -1 });
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static final List<Object> substituteIn(final List<Object> target,
+			final Map<List<Object>, List<Object>> equalities, final Collection<Integer> indices, final int[] index) {
+		final List<Object> replacement = equalities.get(target);
+		
+		if (replacement != null && (indices.isEmpty() || indices.contains(++index[0]))) {
+			return replacement;
+		}
+		
+		return target.stream().map(
+				e -> e instanceof List ? substituteIn((List<Object>) e, equalities, indices, index) : e).collect(toList());
+	}
+	
+}
