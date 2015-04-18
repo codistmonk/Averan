1a46f2712f7700b350e289effc3e55dee4b547f4
codistmonk <codistmonk@4ecbadea-eb05-4a87-8912-100710edbf90>
2015-04-18 21:51:54 +0000

    [Averan2][averan5] Started work on new autoDeduce() tactic.

---
+++ trunk/Averan2/test/averan5/deductions/AutoDeduce.java
@@ -0,0 +1,14 @@
+package averan5.deductions;
+
+import net.sourceforge.aprog.tools.IllegalInstantiationException;
+
+/**
+ * @author codistmonk (creation 2015-04-18)
+ */
+public final class AutoDeduce {
+	
+	private AutoDeduce() {
+		throw new IllegalInstantiationException();
+	}
+	
+}
---
+++ trunk/Averan2/test/averan5/deductions/AutoDeduceTest.java
@@ -0,0 +1,119 @@
+package averan5.deductions;
+
+import static averan5.core.AveranTools.*;
+import static averan5.deductions.Standard.*;
+import static averan5.deductions.StandardTest.build;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author codistmonk (creation 2015-04-18)
+ */
+public final class AutoDeduceTest {
+	
+	@Test
+	public final void testJustify1() {
+		build(() -> {
+			supposeRewrite();
+			deduceIdentity();
+			deduceRecall();
+			
+			suppose($("a"));
+			
+			assertTrue(autoDeduce($("a")));
+		});
+	}
+	
+	@Test
+	public final void testJustify2() {
+		build(() -> {
+			supposeRewrite();
+			deduceIdentity();
+			deduceRecall();
+			
+			suppose($forall("a", "a"));
+			
+			assertTrue(autoDeduce($forall("b", "b")));
+		});
+	}
+	
+	@Test
+	public final void testJustify3() {
+		build(() -> {
+			suppose($rule("a", "b"));
+			suppose($("a"));
+			
+			assertTrue(autoDeduce($("b")));
+		});
+	}
+	
+	@Test
+	public final void testJustify4() {
+		build(() -> {
+			suppose($rule("a", "b"));
+			suppose($rule("b", "c"));
+			suppose($("a"));
+			
+			assertTrue(autoDeduce($("c")));
+		});
+	}
+	
+	@Test
+	public final void testJustify5() {
+		build(() -> {
+			suppose($rule("a", "b", "c"));
+			suppose($("a"));
+			suppose($("b"));
+			
+			assertTrue(autoDeduce($("c")));
+		});
+	}
+	
+	@Test
+	public final void testJustify6() {
+		build(() -> {
+			supposeRewrite();
+			deduceIdentity();
+			deduceRecall();
+			
+			assertFalse(autoDeduce($("b")));
+		});
+	}
+	
+	@Test
+	public final void testJustify7() {
+		build(() -> {
+			suppose($forall("a", "a"));
+			
+			assertTrue(autoDeduce($("b")));
+		});
+	}
+	
+	@Test
+	public final void testJustify8() {
+		build(() -> {
+			suppose($forall("b", $rule("a", "b")));
+			suppose($("a"));
+			
+			assertTrue(autoDeduce($("c")));
+		});
+	}
+	
+	@Test
+	public final void testJustify9() {
+		build(() -> {
+			suppose($forall("b", $rule($equality("a", "b"), "c")));
+			suppose($equality("a", "d"));
+			
+			assertTrue(autoDeduce($("c")));
+		});
+	}
+	
+	public static final boolean autoDeduce(final Object goal) {
+		fail("Not yet implemented");
+		
+		return false;
+	}
+	
+}
